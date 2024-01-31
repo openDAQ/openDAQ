4e5dd950c96da9d97def32d2da78f228f63697e6
Denis Erokhin <denis.erokhin@opendaq.com>
2024-01-31 12:18:10 +0100

    update bindings

--- bindings/python/opendaq/generated/context/py_context.cpp
+++ bindings/python/opendaq/generated/context/py_context.cpp
@@ -79,6 +79,14 @@ void defineIContext(pybind11::module_ m, PyDaqIntf<daq::IContext, daq::IBaseObje
             return objectPtr.getOnCoreEvent().detach();
         },
         py::return_value_policy::take_ownership,
-        "Gets the Core Event object that triggers whenever a change happens within the SDK core structure.");
+        "Gets the Core Event object that triggers whenever a change happens within the openDAQ core structure.");
     */
+    cls.def_property_readonly("options",
+        [](daq::IContext *object)
+        {
+            const auto objectPtr = daq::ContextPtr::Borrow(object);
+            return objectPtr.getOptions().detach();
+        },
+        py::return_value_policy::take_ownership,
+        "Gets the dictionary of module options");
 }

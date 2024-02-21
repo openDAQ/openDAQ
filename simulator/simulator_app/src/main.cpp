99682c3c91ba60d729b02ab7a184d8b65eb40b4b
Denis Erokhin <denis.erokhin@opendaq.com>
2024-02-21 15:46:24 +0100

    debug print

--- simulator/simulator_app/src/main.cpp
+++ simulator/simulator_app/src/main.cpp
@@ -1,11 +1,15 @@
 #include <chrono>
 #include <thread>
 #include <opendaq/opendaq.h>
+#include <iostream>
 
 using namespace daq;
-int main(int /*argc*/, const char* /*argv*/[])
+int main(int argc, const char* argv[])
 {
     using namespace std::chrono_literals;
+
+    for (int i = 0; i < argc; i++)
+        std::cout << "arg[" << i << "] = \"" << argv[i] << "\"\n";
 
     const StringPtr configPath = "/home/opendaq/opendaq/opendaq-config.json";
     const ConfigProviderPtr configProvider = JsonConfigProvider(configPath);

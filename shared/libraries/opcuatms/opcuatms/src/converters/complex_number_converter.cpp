af1c02435dbf4715d8656fd83370ac1fc42341e3
Aljaž <aljaz.francic@gmail.com>
2024-03-04 15:08:14 +0100

    Remove some redundancies

--- shared/libraries/opcuatms/opcuatms/src/converters/complex_number_converter.cpp
+++ shared/libraries/opcuatms/opcuatms/src/converters/complex_number_converter.cpp
@@ -7,23 +7,23 @@ BEGIN_NAMESPACE_OPENDAQ_OPCUA_TMS
 
 // Template specializations
 
-template class StructConverter<IComplexNumber, UA_ComplexNumberType>;
+template class StructConverter<IComplexNumber, UA_DoubleComplexNumberType>;
 template class VariantConverter<IComplexNumber>;
 
-// UA_ComplexNumberType
+// UA_DoubleComplexNumberType
 
 template <>
-ComplexNumberPtr StructConverter<IComplexNumber, UA_ComplexNumberType>::ToDaqObject(const UA_ComplexNumberType& tmsStruct,
+ComplexNumberPtr StructConverter<IComplexNumber, UA_DoubleComplexNumberType>::ToDaqObject(const UA_DoubleComplexNumberType& tmsStruct,
                                                                                     const ContextPtr& /*context*/)
 {
     return ComplexNumber(tmsStruct.real, tmsStruct.imaginary);
 }
 
 template <>
-OpcUaObject<UA_ComplexNumberType> StructConverter<IComplexNumber, UA_ComplexNumberType>::ToTmsType(const ComplexNumberPtr& object,
+OpcUaObject<UA_DoubleComplexNumberType> StructConverter<IComplexNumber, UA_DoubleComplexNumberType>::ToTmsType(const ComplexNumberPtr& object,
                                                                                                    const ContextPtr& /*context*/)
 {
-    OpcUaObject<UA_ComplexNumberType> complex;
+    OpcUaObject<UA_DoubleComplexNumberType> complex;
     complex->real = object.getReal();
     complex->imaginary = object.getImaginary();
     return complex;
@@ -36,10 +36,10 @@ ComplexNumberPtr VariantConverter<IComplexNumber>::ToDaqObject(const OpcUaVarian
 {
     const auto decodedVariant = DecodeIfExtensionObject(variant);
 
-    if (!decodedVariant.isType<UA_ComplexNumberType>())
+    if (!decodedVariant.isType<UA_DoubleComplexNumberType>())
         throw ConversionFailedException();
 
-    return StructConverter<IComplexNumber, UA_ComplexNumberType>::ToDaqObject(*static_cast<UA_ComplexNumberType*>(decodedVariant->data));
+    return StructConverter<IComplexNumber, UA_DoubleComplexNumberType>::ToDaqObject(*static_cast<UA_DoubleComplexNumberType*>(decodedVariant->data));
 }
 
 template <>
@@ -50,7 +50,7 @@ OpcUaVariant VariantConverter<IComplexNumber>::ToVariant(const ComplexNumberPtr&
     auto variant = OpcUaVariant();
 
     if (targetType == nullptr || targetType == &UA_TYPES[UA_TYPES_COMPLEXNUMBERTYPE])
-        variant.setScalar(*StructConverter<IComplexNumber, UA_ComplexNumberType>::ToTmsType(object));
+        variant.setScalar(*StructConverter<IComplexNumber, UA_DoubleComplexNumberType>::ToTmsType(object));
     else
         throw ConversionFailedException{};
 
@@ -60,7 +60,7 @@ OpcUaVariant VariantConverter<IComplexNumber>::ToVariant(const ComplexNumberPtr&
 template <>
 ListPtr<IComplexNumber> VariantConverter<IComplexNumber>::ToDaqList(const OpcUaVariant& variant, const ContextPtr& /*context*/)
 {
-    return ListConversionUtils::VariantToList<IComplexNumber, UA_ComplexNumberType>(variant);
+    return ListConversionUtils::VariantToList<IComplexNumber, UA_DoubleComplexNumberType>(variant);
 }
 
 template <>
@@ -69,7 +69,7 @@ OpcUaVariant VariantConverter<IComplexNumber>::ToArrayVariant(const ListPtr<ICom
                                                               const ContextPtr& /*context*/)
 {
     if (targetType == nullptr || targetType == &UA_TYPES[UA_TYPES_COMPLEXNUMBERTYPE])
-        return ListConversionUtils::ToArrayVariant<IComplexNumber, UA_ComplexNumberType>(list);
+        return ListConversionUtils::ToArrayVariant<IComplexNumber, UA_DoubleComplexNumberType>(list);
 
     throw ConversionFailedException{};
 }

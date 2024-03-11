c441d1e2cce573c55961cefb5d6eec669fe323b9
Yaroslav Popov <yaroslav.k26@gmail.com>
2024-03-11 22:57:00 +0700

    Make Enumeration and EnumerationType behave close to native Python enums Tests update

--- bindings/python/core_types/generated/py_enumeration_type.cpp
+++ bindings/python/core_types/generated/py_enumeration_type.cpp
@@ -30,7 +30,7 @@
 
 PyDaqIntf<daq::IEnumerationType, daq::IType> declareIEnumerationType(pybind11::module_ m)
 {
-    return wrapInterface<daq::IEnumerationType, daq::IType>(m, "IEnumerationType");
+    return wrapInterface<daq::IEnumerationType, daq::IType>(m, "IEnumerationType", py::dynamic_attr());
 }
 
 void defineIEnumerationType(pybind11::module_ m, PyDaqIntf<daq::IEnumerationType, daq::IType> cls)
@@ -71,4 +71,63 @@ void defineIEnumerationType(pybind11::module_ m, PyDaqIntf<daq::IEnumerationType
             return objectPtr.getCount();
         },
         "Gets the number of enumerators within the Enumeration Type.");
+    cls.def("__getattr__", [](daq::IEnumerationType *object, const std::string &value) {
+        const auto objectPtr = daq::EnumerationTypePtr::Borrow(object);
+        
+        daq::DictPtr<daq::IString, daq::IInteger> dict = objectPtr.getAsDictionary();
+        if(!dict.hasKey(value)) {
+            throw py::attribute_error("EnumType has no value named " + value);
+        }
+
+        auto pyObject = py::cast(InterfaceWrapper<daq::IEnumerationType>(objectPtr.addRefAndReturn()));
+        auto typeManager = pyObject.attr("__type_manager").cast<daq::ITypeManager*>();
+        const auto typeManagerPtr = daq::TypeManagerPtr::Borrow(typeManager);
+        const auto enumName = objectPtr.getName();
+
+        return daq::Enumeration_Create(enumName, daq::String(value), typeManagerPtr);
+    });
+    cls.def("__dir__", [](daq::IEnumerationType *object) {
+        const auto objectPtr = daq::EnumerationTypePtr::Borrow(object);
+        auto keys = objectPtr.getAsDictionary().getKeyList();
+
+        // Add method names to the list of keys
+        keys->pushBack(daq::String("enumerator_names"));
+        keys->pushBack(daq::String("as_dictionary"));
+        keys->pushBack(daq::String("get_enumerator_int_value"));
+        keys->pushBack(daq::String("count"));
+
+        return baseObjectToPyObject(keys);
+    });
+    cls.def("__getitem__", [](daq::IEnumerationType *object, const std::string &value) {
+        const auto objectPtr = daq::EnumerationTypePtr::Borrow(object);
+
+        daq::DictPtr<daq::IString, daq::IInteger> dict = objectPtr.getAsDictionary();
+        if(!dict.hasKey(value)) {
+            throw py::key_error("EnumType has no value named " + value);
+        }
+
+        auto pyObject = py::cast(InterfaceWrapper<daq::IEnumerationType>(objectPtr.addRefAndReturn()));
+        auto typeManager = pyObject.attr("__type_manager").cast<daq::ITypeManager*>();
+        const auto typeManagerPtr = daq::TypeManagerPtr::Borrow(typeManager);
+        const auto enumName = objectPtr.getName();
+
+        return daq::Enumeration_Create(enumName, daq::String(value), typeManagerPtr);
+    });
+    cls.def("__call__", [](daq::IEnumerationType *object, int value) {
+        const auto objectPtr = daq::EnumerationTypePtr::Borrow(object);
+
+        daq::DictPtr<daq::IString, daq::IInteger> dict = objectPtr.getAsDictionary();
+        for(const auto& pair : dict) {
+            if(pair.second == value) {
+                auto pyObject = py::cast(InterfaceWrapper<daq::IEnumerationType>(objectPtr.addRefAndReturn()));
+                auto typeManager = pyObject.attr("__type_manager").cast<daq::ITypeManager*>();
+                const auto typeManagerPtr = daq::TypeManagerPtr::Borrow(typeManager);
+                const auto enumName = objectPtr.getName();
+
+                return daq::Enumeration_Create(enumName, pair.first, typeManagerPtr);
+            }
+        }
+
+        throw py::value_error("EnumType has no value with value " + std::to_string(value));
+    });
 }

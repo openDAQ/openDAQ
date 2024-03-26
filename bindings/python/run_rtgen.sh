893972c3090c6043354edcd61515198435d285ae
Jaka Mohorko <jaka.mohorko@dewesoft.com>
2024-03-26 14:36:51 +0100

    Update python bindings

--- bindings/python/run_rtgen.sh
+++ bindings/python/run_rtgen.sh
@@ -128,6 +128,7 @@ run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     
 run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     streaming_info
 run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     streaming_info_config
 run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     mirrored_signal_config
+run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     mirrored_signal_private
 run_rtgen opendaq opendaq/streaming     opendaq opendaq/generated/streaming     subscription_event_args
 run_rtgen opendaq opendaq/component     opendaq opendaq/generated/component     tags
 run_rtgen opendaq opendaq/component     opendaq opendaq/generated/component     tags_private

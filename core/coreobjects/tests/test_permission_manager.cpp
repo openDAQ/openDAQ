e9ea03966e0b9ca0541c14e0ae828bae058e6fbf
Jan Mikolič <jan.mikolic@dewesoft.com>
2024-04-05 18:48:35 +0200

    Add permission mask builder

--- core/coreobjects/tests/test_permission_manager.cpp
+++ core/coreobjects/tests/test_permission_manager.cpp
@@ -3,6 +3,7 @@
 #include <coreobjects/permission_manager_internal_ptr.h>
 #include <coreobjects/user_factory.h>
 #include <coreobjects/permissions_builder_factory.h>
+#include <coreobjects/permission_mask_builder_factory.h>
 
 using namespace daq;
 
@@ -30,10 +31,8 @@ TEST_F(PermissionManagerTest, IsAuthorizedSimple)
     auto guest = User("guest", "password", List<IString>("guest"));
 
     auto manager = PermissionManager();
-    manager.setPermissions(PermissionsBuilder()
-                               .set("admin", List<Permission>(Permission::Read, Permission::Write))
-                               .set("user", List<Permission>(Permission::Read))
-                               .build());
+    manager.setPermissions(
+        PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).set("user", PermissionMaskBuilder().read()).build());
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Write));
@@ -54,10 +53,10 @@ TEST_F(PermissionManagerTest, IsAuthorizedInherited)
     auto user = User("user", "password", List<IString>("user"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
-    manager.setPermissions(PermissionsBuilder().inherit(true).set("user", List<Permission>(Permission::Read)).build());
+    manager.setPermissions(PermissionsBuilder().inherit(true).set("user", PermissionMaskBuilder().read()).build());
 
     ASSERT_TRUE(managerRoot.isAuthorized(admin, Permission::Read));
     ASSERT_TRUE(managerRoot.isAuthorized(admin, Permission::Write));
@@ -78,11 +77,10 @@ TEST_F(PermissionManagerTest, IsAuthorizedNotInherited)
     auto user = User("user", "password", List<IString>("user"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
-    manager.setPermissions(
-        PermissionsBuilder().inherit(false).set("user", List<Permission>(Permission::Read, Permission::Execute)).build());
+    manager.setPermissions(PermissionsBuilder().inherit(false).set("user", PermissionMaskBuilder().read().execute()).build());
 
     ASSERT_TRUE(managerRoot.isAuthorized(admin, Permission::Read));
     ASSERT_TRUE(managerRoot.isAuthorized(admin, Permission::Write));
@@ -102,7 +100,7 @@ TEST_F(PermissionManagerTest, UpdateInherited)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
     manager.setPermissions(PermissionsBuilder().inherit(true).build());
@@ -111,7 +109,7 @@ TEST_F(PermissionManagerTest, UpdateInherited)
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Write));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Execute));
 
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read()).build());
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Write));
@@ -123,10 +121,10 @@ TEST_F(PermissionManagerTest, Allow)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
-    manager.setPermissions(PermissionsBuilder().inherit(true).allow("admin", List<Permission>(Permission::Execute)).build());
+    manager.setPermissions(PermissionsBuilder().inherit(true).allow("admin", PermissionMaskBuilder().execute()).build());
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Write));
@@ -138,10 +136,10 @@ TEST_F(PermissionManagerTest, Deny)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
-    manager.setPermissions(PermissionsBuilder().inherit(true).deny("admin", List<Permission>(Permission::Write)).build());
+    manager.setPermissions(PermissionsBuilder().inherit(true).deny("admin", PermissionMaskBuilder().write()).build());
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Write));
@@ -153,7 +151,7 @@ TEST_F(PermissionManagerTest, UpdateAllowRoot)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
     manager.setPermissions(PermissionsBuilder().inherit(true).build());
@@ -162,7 +160,7 @@ TEST_F(PermissionManagerTest, UpdateAllowRoot)
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Write));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Execute));
 
-    managerRoot.setPermissions(PermissionsBuilder().allow("admin", List<Permission>(Permission::Execute)).build());
+    managerRoot.setPermissions(PermissionsBuilder().allow("admin", PermissionMaskBuilder().execute()).build());
 
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Write));
@@ -174,14 +172,14 @@ TEST_F(PermissionManagerTest, SetParent)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto manager = PermissionManager();
-    manager.setPermissions(PermissionsBuilder().inherit(true).allow("admin", List<Permission>(Permission::Read)).build());
+    manager.setPermissions(PermissionsBuilder().inherit(true).allow("admin", PermissionMaskBuilder().read()).build());
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Write));
     ASSERT_FALSE(manager.isAuthorized(admin, Permission::Execute));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().allow("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().allow("admin", PermissionMaskBuilder().read().write()).build());
     manager.asPtr<IPermissionManagerInternal>().setParent(managerRoot);
 
     ASSERT_TRUE(manager.isAuthorized(admin, Permission::Read));
@@ -194,10 +192,10 @@ TEST_F(PermissionManagerTest, ChangeParent)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot1 = PermissionManager();
-    managerRoot1.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot1.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto managerRoot2 = PermissionManager();
-    managerRoot2.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read)).build());
+    managerRoot2.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read()).build());
 
     auto manager = PermissionManager(managerRoot1);
     manager.setPermissions(PermissionsBuilder().inherit(true).build());
@@ -218,7 +216,7 @@ TEST_F(PermissionManagerTest, SetNullParent)
     auto admin = User("admin", "password", List<IString>("admin", "guest"));
 
     auto managerRoot = PermissionManager();
-    managerRoot.setPermissions(PermissionsBuilder().set("admin", List<Permission>(Permission::Read, Permission::Write)).build());
+    managerRoot.setPermissions(PermissionsBuilder().set("admin", PermissionMaskBuilder().read().write()).build());
 
     auto manager = PermissionManager(managerRoot);
     manager.setPermissions(PermissionsBuilder().inherit(true).build());
--- core/coreobjects/tests/test_permissions_builder.cpp
+++ core/coreobjects/tests/test_permissions_builder.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <coreobjects/permissions_builder_factory.h>
 #include <coreobjects/permission_manager.h>
+#include <coreobjects/permission_mask_builder_factory.h>
 
 using namespace daq;
 
@@ -26,8 +27,7 @@ TEST_F(PermissionsBuilderTest, Inherited)
 TEST_F(PermissionsBuilderTest, Set)
 {
     auto builder = PermissionsBuilder();
-    auto config =
-        builder.set("admin", List<Permission>(Permission::Read, Permission::Write)).set("user", List<Permission>(Permission::Read)).build();
+    auto config = builder.set("admin", PermissionMaskBuilder().read().write()).set("user", PermissionMaskBuilder().read()).build();
     Int mask;
 
     ASSERT_EQ(config.getAllowed().getCount(), 2);
@@ -46,9 +46,7 @@ TEST_F(PermissionsBuilderTest, Set)
 TEST_F(PermissionsBuilderTest, SetOverwrite)
 {
     auto builder = PermissionsBuilder();
-    auto config = builder.set("admin", List<Permission>(Permission::Read, Permission::Write))
-                      .set("admin", List<Permission>(Permission::Read))
-                      .build();
+    auto config = builder.set("admin", PermissionMaskBuilder().read().write()).set("admin", PermissionMaskBuilder().read()).build();
 
     Int mask = config.getAllowed().get("admin");
     ASSERT_EQ(mask, (Int) Permission::Read);
@@ -57,7 +55,7 @@ TEST_F(PermissionsBuilderTest, SetOverwrite)
 TEST_F(PermissionsBuilderTest, Allow)
 {
     auto builder = PermissionsBuilder();
-    auto config = builder.set("user", List<Permission>(Permission::Read)).allow("user", List<Permission>(Permission::Write)).build();
+    auto config = builder.set("user", PermissionMaskBuilder().read()).allow("user", PermissionMaskBuilder().write()).build();
     Int mask;
 
     mask = config.getAllowed().get("user");
@@ -70,7 +68,7 @@ TEST_F(PermissionsBuilderTest, Allow)
 TEST_F(PermissionsBuilderTest, Deny)
 {
     auto builder = PermissionsBuilder();
-    auto config = builder.set("user", List<Permission>(Permission::Read)).deny("user", List<Permission>(Permission::Read)).build();
+    auto config = builder.set("user", PermissionMaskBuilder().read()).deny("user", PermissionMaskBuilder().read()).build();
     Int mask;
 
     mask = config.getAllowed().get("user");
@@ -83,7 +81,7 @@ TEST_F(PermissionsBuilderTest, Deny)
 TEST_F(PermissionsBuilderTest, AllowDeny)
 {
     auto builder = PermissionsBuilder();
-    auto config = builder.allow("user", List<Permission>(Permission::Read)).deny("user", List<Permission>(Permission::Read)).build();
+    auto config = builder.allow("user", PermissionMaskBuilder().read()).deny("user", PermissionMaskBuilder().read()).build();
     Int mask;
 
     mask = config.getAllowed().get("user");
@@ -96,7 +94,7 @@ TEST_F(PermissionsBuilderTest, AllowDeny)
 TEST_F(PermissionsBuilderTest, DenyAllow)
 {
     auto builder = PermissionsBuilder();
-    auto config = builder.deny("user", List<Permission>(Permission::Read)).allow("user", List<Permission>(Permission::Read)).build();
+    auto config = builder.deny("user", PermissionMaskBuilder().read()).allow("user", PermissionMaskBuilder().read()).build();
     Int mask;
 
     mask = config.getAllowed().get("user");
@@ -108,10 +106,10 @@ TEST_F(PermissionsBuilderTest, DenyAllow)
 
 TEST_F(PermissionsBuilderTest, ExtendSimple)
 {
-    auto additionalConfig = PermissionsBuilder().allow("manager", List<Permission>(Permission::Execute)).build();
+    auto additionalConfig = PermissionsBuilder().allow("manager", PermissionMaskBuilder().execute()).build();
 
     auto builder = PermissionsBuilder();
-    auto config = builder.set("admin", List<Permission>(Permission::Read, Permission::Write)).extend(additionalConfig).build();
+    auto config = builder.set("admin", PermissionMaskBuilder().read().write()).extend(additionalConfig).build();
     Int mask;
 
     mask = config.getAllowed().get("admin");
@@ -128,16 +126,16 @@ TEST_F(PermissionsBuilderTest, ExtendSimple)
 TEST_F(PermissionsBuilderTest, Extend)
 {
     auto additionalConfig = PermissionsBuilder()
-                                .deny("admin", List<Permission>(Permission::Write))
-                                .allow("admin", List<Permission>(Permission::Execute))
-                                .allow("user", List<Permission>(Permission::Write))
-                                .allow("manager", List<Permission>(Permission::Write))
-                                .set("guest", List<Permission>(Permission::Read))
+                                .deny("admin", PermissionMaskBuilder().write())
+                                .allow("admin", PermissionMaskBuilder().execute())
+                                .allow("user", PermissionMaskBuilder().write())
+                                .allow("manager", PermissionMaskBuilder().write())
+                                .set("guest", PermissionMaskBuilder().read())
                                 .build();
 
     auto builder = PermissionsBuilder();
-    auto config = builder.set("admin", List<Permission>(Permission::Read, Permission::Write))
-                      .set("user", List<Permission>(Permission::Read))
+    auto config = builder.set("admin", PermissionMaskBuilder().read().write())
+                      .set("user", PermissionMaskBuilder().read())
                       .extend(additionalConfig)
                       .build();
     Int mask;

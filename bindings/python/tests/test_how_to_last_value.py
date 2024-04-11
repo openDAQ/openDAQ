36a7331d086b6a87fc0f8e0e171a66e422003f7b
Aljaž <aljaz.francic@gmail.com>
2024-04-11 12:03:13 +0200

    Cut test for C++ down to one example, add a similar test in Python and add it to CMakeLists.txt, make the how-to code identical to the actual test code

---
+++ bindings/python/tests/test_how_to_last_value.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import unittest
+
+import opendaq_test
+import opendaq
+
+
+# Corresponding document: Antora/modules/howto_guides/pages/howto_measure_single_value.adoc
+class TestDocumentationHowToLastValue(opendaq_test.TestCase):
+    def test_last_value_signal(self):
+        mock = opendaq.MockSignal()
+        mock.add_data([0.1, 0.2, 0.3])
+        signal = mock.signal
+
+        # START DOCS CODE
+
+        # Get last value of a Signal
+        last_value = signal.last_value
+
+        # END DOCS CODE
+
+        self.assertEqual(last_value, 0.3)
+
+
+if __name__ == '__main__':
+    unittest.main()

fe6059d6ff2e870ec04b4991bc4c8b4f63dc1cdd
Aljaž <aljaz.francic@gmail.com>
2024-04-24 13:36:43 +0200

    Update `test_app.cpp` to avoid false memory leak detections

--- regression/test_app.cpp
+++ regression/test_app.cpp
@@ -1,8 +1,17 @@
-#include <testutils/testutils.h>
+#include <coreobjects/util.h>
+#include <coretypes/stringobject_factory.h>
+#include <opendaq/log.h>
+#include <opendaq/module_manager_init.h>
+#include <opendaq/opendaq_init.h>
 #include <testutils/bb_memcheck_listener.h>
+#include <testutils/testutils.h>
 
 int main(int argc, char** args)
 {
+    daq::daqInitializeCoreObjectsTesting();
+    daqInitModuleManagerLibrary();
+    daqInitOpenDaqLibrary();
+
     testing::InitGoogleTest(&argc, args);
 
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();

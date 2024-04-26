39552bbc81d7986a314dd99f7035e00e402dd193
Aljaž <aljaz.francic@gmail.com>
2024-04-26 15:39:24 +0200

    Add the rest of Property regression tests

--- regression/regression_test_property.cpp
+++ regression/regression_test_property.cpp
@@ -66,12 +66,73 @@ TEST_P(RegressionTestProperty, getMaxValue)
 {
     ASSERT_NO_THROW(property.getMaxValue());
 }
-/*
-TEST_P(RegressionTestProperty, )
+
+TEST_P(RegressionTestProperty, getDefaultValue)
 {
-    ASSERT_NO_THROW(property.());
+    ASSERT_NO_THROW(property.getDefaultValue());
 }
-*/
+
+TEST_P(RegressionTestProperty, getSuggestedValues)
+{
+    ASSERT_NO_THROW(property.getSuggestedValues());
+}
+
+TEST_P(RegressionTestProperty, getVisible)
+{
+    ASSERT_NO_THROW(property.getVisible());
+}
+
+TEST_P(RegressionTestProperty, getReadOnly)
+{
+    ASSERT_NO_THROW(property.getReadOnly());
+}
+
+TEST_P(RegressionTestProperty, getSelectionValues)
+{
+    ASSERT_NO_THROW(property.getSelectionValues());
+}
+
+TEST_P(RegressionTestProperty, getReferencedProperty)
+{
+    ASSERT_NO_THROW(property.getReferencedProperty());
+}
+
+TEST_P(RegressionTestProperty, getIsReferenced)
+{
+    ASSERT_NO_THROW(property.getIsReferenced());
+}
+
+TEST_P(RegressionTestProperty, getValidator)
+{
+    ASSERT_NO_THROW(property.getValidator());
+}
+
+TEST_P(RegressionTestProperty, getCoercer)
+{
+    ASSERT_NO_THROW(property.getCoercer());
+}
+
+TEST_P(RegressionTestProperty, getCallableInfo)
+{
+    ASSERT_NO_THROW(property.getCallableInfo());
+}
+
+// TODO enable
+TEST_P(RegressionTestProperty, DISABLED_getStructType)
+{
+    ASSERT_NO_THROW(property.getStructType());
+}
+
+TEST_P(RegressionTestProperty, getOnPropertyValueWrite)
+{
+    ASSERT_NO_THROW(property.getOnPropertyValueWrite());
+}
+
+TEST_P(RegressionTestProperty, getOnPropertyValueRead)
+{
+    ASSERT_NO_THROW(property.getOnPropertyValueRead());
+}
+
 INSTANTIATE_TEST_SUITE_P(Property,
                          RegressionTestProperty,
                          testing::Values("daq.opcua://127.0.0.1", "daq.ns://127.0.0.1", "daq.lt://127.0.0.1"));

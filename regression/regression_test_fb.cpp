c7aab60d5fc09e47a0bf36414eda864f25dbce96
Aljaž <aljaz.francic@gmail.com>
2024-05-06 16:11:22 +0200

    Enhance `regresssion_test_fb.cpp`

--- regression/regression_test_fb.cpp
+++ regression/regression_test_fb.cpp
@@ -30,35 +30,49 @@ protected:
 
 TEST_P(RegressionTestFunctionBlock, getFunctionBlockType)
 {
-    ASSERT_NO_THROW(fb.getFunctionBlockType());
+    FunctionBlockTypePtr type;
+    ASSERT_NO_THROW(type = fb.getFunctionBlockType());
+    ASSERT_EQ(type.getId(), "ref_fb_module_trigger");
+    ASSERT_EQ(type.getName(), "Trigger");
+    ASSERT_EQ(type.getDescription(), "Trigger");
 }
 
 TEST_P(RegressionTestFunctionBlock, getInputPorts)
 {
-    ASSERT_NO_THROW(fb.getInputPorts());
+    ListPtr<IInputPort> ports;
+    ASSERT_NO_THROW(ports = fb.getInputPorts());
+    ASSERT_EQ(ports.getCount(), 1);
 }
 
 TEST_P(RegressionTestFunctionBlock, getSignals)
 {
-    ASSERT_NO_THROW(fb.getSignals());
+    ListPtr<ISignal> signals;
+    ASSERT_NO_THROW(signals = fb.getSignals());
+    ASSERT_EQ(signals.getCount(), 2);
 }
 
 TEST_P(RegressionTestFunctionBlock, getSignalsRecursive)
 {
-    ASSERT_NO_THROW(fb.getSignalsRecursive());
+    ListPtr<ISignal> signals;
+    ASSERT_NO_THROW(signals = fb.getSignalsRecursive());
+    ASSERT_EQ(signals.getCount(), 2);
 }
 
 TEST_P(RegressionTestFunctionBlock, getStatusSignal)
 {
-    ASSERT_NO_THROW(fb.getStatusSignal());
+    SignalPtr status;
+    ASSERT_NO_THROW(status = fb.getStatusSignal());
+    ASSERT_EQ(status, nullptr);
 }
 
 TEST_P(RegressionTestFunctionBlock, getFunctionBlocks)
 {
-    ASSERT_NO_THROW(fb.getFunctionBlocks());
+    ListPtr<IFunctionBlock> fbs;
+    ASSERT_NO_THROW(fbs = fb.getFunctionBlocks());
+    ASSERT_EQ(fbs.getCount(), 0);
 }
 
-// TODO: ???
+// TODO: ??? + no need for TEST_P if only OPC UA
 INSTANTIATE_TEST_SUITE_P(FunctionBlock,
                          RegressionTestFunctionBlock,
                          testing::Values("daq.opcua://127.0.0.1" /*, "daq.ns://127.0.0.1", "daq.lt://127.0.0.1"*/));

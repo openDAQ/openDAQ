3ab759a4fe7e6537f0c22e1840de4c53210bf127
Aljaž <aljaz.francic@gmail.com>
2024-05-08 11:20:21 +0200

    Enhance `regression_test_component.cpp`

--- regression/regression_test_component.cpp
+++ regression/regression_test_component.cpp
@@ -27,87 +27,114 @@ protected:
 
 TEST_P(RegressionTestComponent, getLocalId)
 {
-    ASSERT_NO_THROW(component.getLocalId());
+    StringPtr id;
+    ASSERT_NO_THROW(id = component.getLocalId());
+    StringPtr realId;
+    auto connectionString = GetParam();
+    if (connectionString == "daq.opcua://127.0.0.1")
+        realId = "ref_dev1";
+    else if (connectionString == "daq.ns://127.0.0.1")
+        realId = "streaming_pseudo_device0";
+    else if (connectionString == "daq.lt://127.0.0.1")
+        realId = "websocket_pseudo_device0";
+    ASSERT_EQ(id, realId);
 }
 
 TEST_P(RegressionTestComponent, getGlobalId)
 {
-    ASSERT_NO_THROW(component.getGlobalId());
+    StringPtr id;
+    ASSERT_NO_THROW(id = component.getGlobalId());
+    StringPtr realId;
+    auto connectionString = GetParam();
+    if (connectionString == "daq.opcua://127.0.0.1")
+        realId = "/mock_instance/Dev/ref_dev1";
+    else if (connectionString == "daq.ns://127.0.0.1")
+        realId = "/mock_instance/Dev/streaming_pseudo_device0";
+    else if (connectionString == "daq.lt://127.0.0.1")
+        realId = "/mock_instance/Dev/websocket_pseudo_device0";
+    ASSERT_EQ(id, realId);
 }
 
-TEST_P(RegressionTestComponent, getActive)
-{
-    ASSERT_NO_THROW(component.getActive());
-}
-
-TEST_P(RegressionTestComponent, setActive)
+TEST_P(RegressionTestComponent, setActiveGetActive)
 {
     ASSERT_NO_THROW(component.setActive(True));
+    Bool active;
+    ASSERT_NO_THROW(active = component.getActive());
+    ASSERT_TRUE(active);
 }
 
 TEST_P(RegressionTestComponent, getContext)
 {
-    ASSERT_NO_THROW(component.getContext());
+    ContextPtr context;
+    ASSERT_NO_THROW(context = component.getContext());
+    ASSERT_NE(context, nullptr);
 }
 
 TEST_P(RegressionTestComponent, getParent)
 {
-    ASSERT_NO_THROW(component.getParent());
+    ComponentPtr parent;
+    ASSERT_NO_THROW(parent = component.getParent());
+    ASSERT_EQ(parent.getName(), "Dev");
 }
 
-TEST_P(RegressionTestComponent, getName)
+TEST_P(RegressionTestComponent, setNameGetName)
 {
-    ASSERT_NO_THROW(component.getName());
+    StringPtr newName = "test_name";
+    ASSERT_NO_THROW(component.setName(newName));
+    StringPtr name;
+    ASSERT_NO_THROW(name = component.getName());
+    ASSERT_EQ(name, newName);
 }
 
-TEST_P(RegressionTestComponent, setName)
+TEST_P(RegressionTestComponent, setDescriptionGetDescription)
 {
-    ASSERT_NO_THROW(component.setName("test_name"));
-}
-
-TEST_P(RegressionTestComponent, getDescription)
-{
-    ASSERT_NO_THROW(component.getDescription());
-}
-
-TEST_P(RegressionTestComponent, setDescription)
-{
-    ASSERT_NO_THROW(component.setDescription("test_description"));
+    StringPtr newDescription = "test_description";
+    ASSERT_NO_THROW(component.setDescription(newDescription));
+    StringPtr description;
+    ASSERT_NO_THROW(description = component.getDescription());
+    ASSERT_EQ(description, newDescription);
 }
 
 TEST_P(RegressionTestComponent, getTags)
 {
-    ASSERT_NO_THROW(component.getTags());
+    TagsPtr tags;
+    ASSERT_NO_THROW(tags = component.getTags());
+    ASSERT_NE(tags, nullptr);
 }
 
-TEST_P(RegressionTestComponent, getVisible)
+TEST_P(RegressionTestComponent, setVisibleGetVisible)
 {
-    ASSERT_NO_THROW(component.getVisible());
-}
-
-TEST_P(RegressionTestComponent, setVisible)
-{
-    ASSERT_NO_THROW(component.setVisible(True));
+    ASSERT_NO_THROW(component.setVisible(True));  // TODO: does nothing, because the attribute is locked
+    Bool visible;
+    ASSERT_NO_THROW(visible = component.getVisible());
+    ASSERT_TRUE(visible);
 }
 
 TEST_P(RegressionTestComponent, getLockedAttributes)
 {
-    ASSERT_NO_THROW(component.getLockedAttributes());
+    ListPtr<IString> lockedAttributes;
+    ASSERT_NO_THROW(lockedAttributes = component.getLockedAttributes());
+    ASSERT_EQ(lockedAttributes.getCount(), 1);
 }
 
 TEST_P(RegressionTestComponent, getOnComponentCoreEvent)
 {
-    ASSERT_NO_THROW(component.getOnComponentCoreEvent());
+    auto event = component.getOnComponentCoreEvent();
+    ASSERT_NE(event, nullptr);
 }
 
 TEST_P(RegressionTestComponent, getStatusContainer)
 {
-    ASSERT_NO_THROW(component.getStatusContainer());
+    ComponentStatusContainerPtr container;
+    ASSERT_NO_THROW(container = component.getStatusContainer());
+    ASSERT_NE(container, nullptr);
 }
 
 TEST_P(RegressionTestComponent, findComponent)
 {
-    ASSERT_NO_THROW(component.findComponent("test_component"));
+    ComponentPtr comp;
+    ASSERT_NO_THROW(comp = component.findComponent("Sig"));
+    ASSERT_EQ(comp.getName(), "Sig");
 }
 
 INSTANTIATE_TEST_SUITE_P(Component,

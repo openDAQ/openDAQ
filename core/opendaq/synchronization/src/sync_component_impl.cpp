9cdd48a03f244ca8cdf90887724d976bd0066fed
Daniel Barros <daniel.duraesdebarros@hbkworld.com>
2024-05-09 11:26:12 +0000

    Added empty ISyncComponent Interface

---
+++ core/opendaq/synchronization/src/sync_component_impl.cpp
@@ -0,0 +1,14 @@
+#include <opendaq/sync_component_impl.h>
+
+
+BEGIN_NAMESPACE_OPENDAQ
+
+OPENDAQ_DEFINE_CLASS_FACTORY_WITH_INTERFACE(
+    LIBRARY_FACTORY,
+	SyncComponent,
+	ISyncComponent,
+    IContext*, context,
+    IComponent*, parent,
+    IString*, localId)
+
+END_NAMESPACE_OPENDAQ

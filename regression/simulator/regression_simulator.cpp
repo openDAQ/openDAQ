8dd4dc13e0775c5681b15a50d0798efb015774b5
Aljaž <aljaz.francic@gmail.com>
2024-05-20 16:21:16 +0200

    Introduce `ready` file, add and remove some debugging code

--- regression/simulator/regression_simulator.cpp
+++ regression/simulator/regression_simulator.cpp
@@ -1,5 +1,6 @@
 #include <opendaq/opendaq.h>
 #include <chrono>
+#include <fstream>
 #include <thread>
 
 using namespace daq;
@@ -14,6 +15,16 @@ int main(int /*argc*/, const char* /*argv*/[])
     instance.addServer("openDAQ OpcUa", nullptr);
     instance.addServer("openDAQ Native Streaming", nullptr);
     instance.addServer("openDAQ LT Streaming", nullptr);
+
+    // Create an empty file named "ready" to let regression test suite know
+    // the Simulator is up and running and ready for tests
+    std::fstream fs;
+    fs.open("ready", std::ios::out);
+    fs.close();
+    // Github Action will wait until "ready" file exists,
+    // then start the regression tests
+    // This file is later deleted in GitHub Action via shell
+    // so we can test another prococol
 
     while (true)
         std::this_thread::sleep_for(100ms);

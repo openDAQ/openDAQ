6b3c483d04a1c71d758bc27caf201f7024754991
Denis Erokhin <denis.erokhin@opendaq.com>
2024-05-22 15:27:06 +0200

    IDiscoveryService implementation

---
+++ core/opendaq/modulemanager/include/opendaq/mdns_discovery_service_impl.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022-2024 Blueberry d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <opendaq/discovery_service.h>
+#include <discovery_server/daq_discovery_server.h>
+#include <opendaq/logger_ptr.h>
+
+BEGIN_NAMESPACE_OPENDAQ
+
+class MdnsDiscoveryService : public ImplementationOf<IDiscoveryService>
+{
+public:
+    MdnsDiscoveryService(const LoggerPtr& logger);
+
+    ErrCode INTERFACE_FUNC registerService(IString* id, IPropertyObject* config, IDeviceInfo* deviceInfo) override;
+    ErrCode INTERFACE_FUNC unregisterService(IString* id) override;
+private:
+    discovery_service::DiscoveryServer discoveryServer;
+    LoggerComponentPtr loggerComponent;
+
+};
+
+END_NAMESPACE_OPENDAQ
---
+++ core/opendaq/modulemanager/src/mdns_discovery_service_impl.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022-2024 Blueberry d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <opendaq/mdns_discovery_service_impl.h>
+#include <opendaq/custom_log.h>
+
+BEGIN_NAMESPACE_OPENDAQ
+
+MdnsDiscoveryService::MdnsDiscoveryService(const LoggerPtr& logger)
+    : loggerComponent(logger.getOrAddComponent("MdnsDiscoveryService"))
+{
+}
+
+ErrCode MdnsDiscoveryService::registerService(IString* id, IPropertyObject* config, IDeviceInfo* deviceInfo)
+{
+    if (id == nullptr || config == nullptr)
+        return OPENDAQ_IGNORED;
+
+    if (discoveryServer.registerDevice(id, config, deviceInfo))
+    {
+        LOG_I("Server \"{}\" registered in the discovery service", StringPtr::Borrow(id));
+        return OPENDAQ_SUCCESS;
+    }
+    return OPENDAQ_IGNORED;
+}
+
+ErrCode MdnsDiscoveryService::unregisterService(IString* id)
+{
+    if (id == nullptr)
+        return OPENDAQ_IGNORED;
+
+    if (discoveryServer.removeDevice(id))
+    {
+        LOG_I("Server \"{}\" removed from the discovery service", StringPtr::Borrow(id));
+        return OPENDAQ_SUCCESS;
+    }
+    return OPENDAQ_IGNORED;
+}
+
+END_NAMESPACE_OPENDAQ

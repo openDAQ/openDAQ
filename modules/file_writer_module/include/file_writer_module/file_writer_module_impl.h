de75dac7d41583eb196f15cf6fbbc6bb40f8d757
roettger <nils.roettger@hbkworld.com>
2024-07-11 12:07:56 +0000

    Init file writer module.

---
+++ modules/file_writer_module/include/file_writer_module/file_writer_module_impl.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022-2024 openDAQ d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <file_writer_module/common.h>
+#include <opendaq/module_impl.h>
+
+BEGIN_NAMESPACE_FILE_WRITER_MODULE
+
+class FileWriterModule final : public Module
+{
+public:
+    explicit FileWriterModule(ContextPtr ctx);
+
+    DictPtr<IString, IFunctionBlockType> onGetAvailableFunctionBlockTypes() override;
+    FunctionBlockPtr onCreateFunctionBlock(const StringPtr& id, const ComponentPtr& parent, const StringPtr& localId, const PropertyObjectPtr& config) override;
+private:
+};
+
+END_NAMESPACE_FILE_WRITER_MODULE
---
+++ modules/file_writer_module/src/module_dll.cpp
@@ -0,0 +1,9 @@
+#include <file_writer_module/module_dll.h>
+#include <file_writer_module/file_writer_module_impl.h>
+
+#include <opendaq/module_factory.h>
+
+using namespace daq::modules::file_writer_module;
+
+DEFINE_MODULE_EXPORTS(FileWriterModule)
+

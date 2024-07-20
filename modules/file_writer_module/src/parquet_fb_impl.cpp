d3bca335b75498e65c2cf111a8a720d628f1ee19
nilsRoettgerAtBB <nils.roettger@hbkworld.com>
2024-07-20 12:51:44 +0000

    Add parquet headers.

--- modules/file_writer_module/src/parquet_fb_impl.cpp
+++ modules/file_writer_module/src/parquet_fb_impl.cpp
@@ -21,6 +21,12 @@
 #include <opendaq/reusable_data_packet_ptr.h>
 #include <opendaq/component_status_container_private_ptr.h>
 
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+#include <parquet/arrow/writer.h>
+#include <parquet/exception.h>
+
 BEGIN_NAMESPACE_FILE_WRITER_MODULE
 
 namespace FileWriter
@@ -30,12 +36,52 @@ static const char* InputDisconnected = "Disconnected";
 static const char* InputConnected = "Connected";
 static const char* InputInvalid = "Invalid";
 
+// #0 Build dummy data to pass around
+// To have some input data, we first create an Arrow Table that holds
+// some data.
+static std::shared_ptr<arrow::Table> generate_table() {
+  arrow::Int64Builder i64builder;
+  PARQUET_THROW_NOT_OK(i64builder.AppendValues({1, 2, 3, 4, 5}));
+  std::shared_ptr<arrow::Array> i64array;
+  PARQUET_THROW_NOT_OK(i64builder.Finish(&i64array));
+
+  arrow::StringBuilder strbuilder;
+  PARQUET_THROW_NOT_OK(strbuilder.Append("some"));
+  PARQUET_THROW_NOT_OK(strbuilder.Append("string"));
+  PARQUET_THROW_NOT_OK(strbuilder.Append("content"));
+  PARQUET_THROW_NOT_OK(strbuilder.Append("in"));
+  PARQUET_THROW_NOT_OK(strbuilder.Append("rows"));
+  std::shared_ptr<arrow::Array> strarray;
+  PARQUET_THROW_NOT_OK(strbuilder.Finish(&strarray));
+
+  std::shared_ptr<arrow::Schema> schema = arrow::schema(
+      {arrow::field("int", arrow::int64()), arrow::field("str", arrow::utf8())});
+
+  return arrow::Table::Make(schema, {i64array, strarray});
+}
+
+
+// #1 Write out the data as a Parquet file
+static void write_parquet_file(const arrow::Table& table) {
+  std::shared_ptr<arrow::io::FileOutputStream> outfile;
+  PARQUET_ASSIGN_OR_THROW(
+      outfile, arrow::io::FileOutputStream::Open("parquet-arrow-example.parquet"));
+  // The last argument to the function call is the size of the RowGroup in
+  // the parquet file. Normally you would choose this to be rather large but
+  // for the example, we use a small value to have multiple RowGroups.
+  PARQUET_THROW_NOT_OK(
+      parquet::arrow::WriteTable(table, arrow::default_memory_pool(), outfile, 3));
+}
+
 ParquetFbImpl::ParquetFbImpl(const ContextPtr& ctx, const ComponentPtr& parent, const StringPtr& localId)
     : FunctionBlock(CreateType(), ctx, parent, localId)
 {
     createInputPorts();
     initProperties();
     initStatuses();
+
+    std::shared_ptr<arrow::Table> table = generate_table();
+    write_parquet_file(*table);
 }
 
 void ParquetFbImpl::initProperties()

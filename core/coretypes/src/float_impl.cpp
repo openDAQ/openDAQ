cd5bafd7bf5d582f2307ee11244dfbea50b2b9c1
Dejan Crnila <dejan.crnila@dewesoft.si>
2024-09-10 14:07:10 +0200

    Optimize number of core type objects instantiations

--- core/coretypes/src/float_impl.cpp
+++ core/coretypes/src/float_impl.cpp
@@ -3,13 +3,46 @@
 
 BEGIN_NAMESPACE_OPENDAQ
 
-OPENDAQ_DEFINE_CLASS_FACTORY(LIBRARY_FACTORY, Float, const Float, value)
-OPENDAQ_DEFINE_CLASS_FACTORY_WITH_INTERFACE_AND_CREATEFUNC(
-    LIBRARY_FACTORY,
-    Float, IFloat,
-    createFloatObject,
-    const Float,
-    value
-)
+class StaticFloats
+{
+public:
+    IFloat* zeroValue;
+
+    StaticFloats()
+    {
+        daq::createObject<IFloat, FloatImpl, const Float>(&zeroValue, 0.0);
+    }
+
+    ~StaticFloats()
+    {
+        zeroValue->releaseRef();
+    }
+
+} staticFloats;
+
+inline daq::ErrCode createFloatInternal(IFloat** objTmp, const Float value)
+{
+    if (!objTmp)
+        return OPENDAQ_ERR_ARGUMENT_NULL;
+
+    if (value == 0.0)
+    {
+        staticFloats.zeroValue->addRef();
+        *objTmp = staticFloats.zeroValue;
+        return OPENDAQ_SUCCESS;
+    }
+
+    return daq::createObject<IFloat, FloatImpl, const Float>(objTmp, value);
+}
+
+extern "C" daq::ErrCode LIBRARY_FACTORY createFloat(IFloat** objTmp, const Float value)
+{
+    return createFloatInternal(objTmp, value);
+}
+
+extern "C" daq::ErrCode LIBRARY_FACTORY createFloatObject(IFloat** objTmp, const Float value)
+{
+    return createFloatInternal(objTmp, value);
+}
 
 END_NAMESPACE_OPENDAQ

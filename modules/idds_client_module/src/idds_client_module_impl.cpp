45dba71b2144acfc323611402e7ddc78f97afa92
Daniel Barros <daniel.duraesdebarros@hbkworld.com>
2024-09-16 23:33:37 +0000

    Add barebones structure for iDDSClientModule implementation

---
+++ modules/idds_client_module/src/idds_client_module_impl.cpp
@@ -0,0 +1,41 @@
+#include <idds_client_module/idds_client_module_impl.h>
+#include <idds_client_module/version.h>
+#include <coretypes/version_info_factory.h>
+#include <chrono>
+#include <coreobjects/callable_info_factory.h>
+#include <coreobjects/argument_info_factory.h>
+#include <opendaq/device_type_factory.h>
+#include <opendaq/address_info_factory.h>
+#include <opendaq/device_impl.h>
+
+BEGIN_NAMESPACE_OPENDAQ_IDDS_CLIENT_MODULE
+
+using namespace daq::idds;
+
+iDDSClientModule::iDDSClientModule(ContextPtr context)
+    : Module("OpenDAQiDDSClientModule",
+            daq::VersionInfo(IDDS_CL_MODULE_MAJOR_VERSION, IDDS_CL_MODULE_MINOR_VERSION, IDDS_CL_MODULE_PATCH_VERSION),
+            std::move(context),
+            "OpenDAQiDDSClientModule")
+{
+}
+
+DictPtr<IString, IDeviceType> iDDSClientModule::onGetAvailableDeviceTypes()
+{
+    auto result = Dict<IString, IDeviceType>();
+
+    //TBD: Add device types here
+
+    return result;
+}
+
+DevicePtr iDDSClientModule::onCreateDevice(const StringPtr& connectionString,
+                                                         const ComponentPtr& parent,
+                                                         const PropertyObjectPtr& /*config*/)
+{
+    DevicePtr obj(createWithImplementation<IDevice, Device>(context, parent, "iDDSDevice"));
+
+    return obj;
+}
+
+END_NAMESPACE_OPENDAQ_IDDS_CLIENT_MODULE
---
+++ modules/idds_client_module/src/module_dll.cpp
@@ -0,0 +1,8 @@
+#include <idds_client_module/module_dll.h>
+#include <idds_client_module/idds_client_module_impl.h>
+
+#include <opendaq/module_factory.h>
+
+using namespace daq::modules::idds_client_module;
+
+DEFINE_MODULE_EXPORTS(iDDSClientModule)

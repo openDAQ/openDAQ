fbb5e789a0f6d9a3b28a6013b76e326b69a95b20
Aljaž <aljaz.francic@gmail.com>
2024-10-16 20:11:44 +0200

    Inject version into `quick_start_setting_up_python.adoc` and `quick_start_setting_up_cpp.adoc`

--- docs/version_injector.py
+++ docs/version_injector.py
@@ -9,12 +9,15 @@ def write_file(filename, content):
     with open(filename, "w") as file:
         file.write(content)
 
-# this code is used to inject opqnDAQ version into Antora docs during CI
+def read_and_replace(filename):
+    new= read_file(filename).replace("opendaq_version", version)
+    write_file(filename, new)
 
-f1 = "docs/Antora/antora.yml"
-f2 = "docs/Antora-specs/antora.yml"
+# this code is used to inject opqnDAQ version into Antora docs during CI
 version = read_file("opendaq_version").strip()
-a1 = read_file(f1).replace("opendaq_version", version)
-write_file(f1, a1)
-a2 = read_file(f2).replace("opendaq_version", version)
-write_file(f2, a2)
+
+read_and_replace("docs/Antora/antora.yml")
+read_and_replace("docs/Antora-specs/antora.yml")
+read_and_replace("docs/Antora/modules/getting_started/pages/quick_start_setting_up_cpp.adoc")
+read_and_replace("docs/Antora/modules/getting_started/pages/quick_start_setting_up_python.adoc")
+

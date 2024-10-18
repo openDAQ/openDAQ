cf643e83fcb3dc51bc491d2b0502022e5b5934b5
Aljaž Frančič <aljaz.francic@gmail.com>
2024-10-18 12:49:25 +0200

    Inject openDAQ version from a single file (#550)
    * Use `OPENDAQ_PACKAGE_VERSION` instead of hard-coded version throughout `CMakeLists.txt` files
    * Delete whitespace in `opendaq_verison`
    * Change `antora.yml` to contain a placeholder
    * Add Python `openda_version` injector
    * Add version injection  to `build_antora_docs.yml`
    * Add version injection  to `deploy.yml`
    * Inject version into `quick_start_setting_up_python.adoc` and `quick_start_setting_up_cpp.adoc`
    * `HEAD_DAQ_VERSION` and `HEAD_DOC_VERSION` variables appear to be unused, so this code is obsolete and we can safely delete the whole CI job
    * Minor fixes

---
+++ docs/version_injector.py
@@ -0,0 +1,24 @@
+import sys
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+        return content
+    
+def write_file(filename, content):
+    with open(filename, "w") as file:
+        file.write(content)
+
+def read_and_replace(filename):
+    new = read_file(filename).replace("opendaq_version", version)
+    write_file(filename, new)
+
+# this code is used to inject opqnDAQ version into Antora docs during CI
+
+version = read_file("opendaq_version").strip()
+
+read_and_replace("docs/Antora/antora.yml")
+read_and_replace("docs/Antora-specs/antora.yml")
+read_and_replace("docs/Antora/modules/getting_started/pages/quick_start_setting_up_cpp.adoc")
+read_and_replace("docs/Antora/modules/getting_started/pages/quick_start_setting_up_python.adoc")
+

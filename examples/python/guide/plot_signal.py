ca4b431f821496294be3f6f87ca3f8a13ed2775c
Aljaž <aljaz.francic@gmail.com>
2024-10-25 12:59:06 +0200

    Fix plot signal Python example

--- examples/python/guide/plot_signal.py
+++ examples/python/guide/plot_signal.py
@@ -6,7 +6,10 @@ instance = opendaq.Instance()
 device = instance.add_device('daqref://device0')
 signal = device.signals_recursive[0]
 
-reader = opendaq.StreamReader(signal)
+builder = opendaq.StreamReaderBuilder()
+builder.signal = signal
+builder.skip_events = True
+reader = builder.build()
 
 time.sleep(1.0)
 samples = reader.read(1000)

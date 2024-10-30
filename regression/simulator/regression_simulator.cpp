92dc15b32b758ed22940ce0188d9bff6e87e3982
Aljaž <aljaz.francic@gmail.com>
2024-10-30 05:39:56 +0100

    Commit

--- regression/simulator/regression_simulator.cpp
+++ regression/simulator/regression_simulator.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <iostream>
 #include <coreobjects/argument_info_factory.h>
+#include "../../build/__external/src/streaming_protocol/include/streaming_protocol/Types.h"
+#include "opendaq/device_domain_factory.h"
 
 int main (int /*argc*/, const char* /*argv*/[])
 {
@@ -46,55 +48,58 @@ int main (int /*argc*/, const char* /*argv*/[])
     // Add custom Reference Property
     instance.addProperty(ReferenceProperty("TestReference", daq::EvalValue("%TestString")));
     // Add custom Struct Property
-    instance.addProperty(StructProperty("TestStruct", Struct("TestName", dict, TypeManager())));
+    instance.addProperty(StructProperty("TestStruct", Struct("TestName", dict, daq::TypeManager())));
     // Add custom Argument Info Property
-    auto argInfo = ArgumentInfo(String("TestArgInfo"), CoreType::ctInt);
-    auto argInfoProp = PropertyBuilder("TestArgInfoProp").setDefaultValue(argInfo).setValueType(CoreType::ctStruct).build();
+    auto argInfo = (nullptr, daq::CoreType::ctInt);
+    auto argInfoProp = daq::PropertyBuilder("TestArgInfoProp").setDefaultValue(argInfo).setValueType(daq::CoreType::ctInt).build();
     instance.addProperty(argInfoProp);
     // Add custom Callable Info Property
-    auto callInfo = CallableInfo_Create(list, CoreType::ctInt, false);
-    auto callInfoProp = PropertyBuilder("TestCallInfoProp").setDefaultValue(callInfo).setValueType(CoreType::ctStruct).build();
+    auto callInfo = CallableInfo_Create(list, daq::CoreType::ctInt, false);
+    auto callInfoProp = daq::PropertyBuilder("TestCallInfoProp").setDefaultValue(callInfo).setValueType(daq::CoreType::ctString).build();
     instance.addProperty(callInfoProp);
     // Add custom Unit Property
-    auto unit = Unit("TestSymbol", -1, "TestName", "TestQuantity");
-    auto unitProp = PropertyBuilder("TestUnitProp").setDefaultValue(unit).setValueType(CoreType::ctStruct).build();
+    auto unit = daq::Unit("TestSymbol", -1, "TestName", "TestQuantity");
+    auto unitProp = daq::PropertyBuilder("TestUnitProp").setDefaultValue(unit).setValueType(daq::ctStruct).build();
     instance.addProperty(unitProp);
     // Add custom Complex Number Property
-    auto complex = ComplexFloat32(31, 32);
-    auto complexProp = PropertyBuilder("TestComplexProp").setDefaultValue(complex).setValueType(CoreType::ctComplexNumber).build();
+    auto complex = daq::ComplexFloat32(31, 32);
+    auto complexProp = daq::PropertyBuilder("TestComplexProp").setDefaultValue(complex).setValueType(daq::CoreType::ctComplexNumber).build();
     instance.addProperty(complexProp);
     // Add custom Ratio Property
-    auto ratioProp = RatioProperty("TestRatioProp", Ratio(3, 4));
+    auto ratioProp = daq::RatioProperty("TestRatioProp", daq::Ratio(3, 4));
     instance.addProperty(ratioProp);
     // Add custom Function Block Type Property
-    auto fbType = FunctionBlockType("TestFunctionBlockId", "TestFunctionBlockName", "TestFunctionBlockDesc");
-    auto fbTypeProp = PropertyBuilder("TestFunctionBlockProp").setDefaultValue(fbType).setValueType(CoreType::ctStruct).build();
+    auto fbType = daq::FunctionBlockType("TestFunctionBlockId", "TestFunctionBlockName", "TestFunctionBlockDesc");
+    auto fbTypeProp = daq::PropertyBuilder("TestFunctionBlockProp").setDefaultValue(fbType).setValueType(daq::ctStruct).build();
     instance.addProperty(fbTypeProp);
     // Add custom Data Descriptor Property
-    auto dims = List<IDimension>();
-    dims.pushBack(Dimension(LinearDimensionRule(1, 2, 3), unit, String("TestDimensionName")));
-    auto dataDesc = DataDescriptorBuilder()
-                        .setDimensions(dims)
-                        //.setMetadata()
-                        .setName("TestDataDescriptor")
-                        .setOrigin("1970")
-                        //.setPostScaling(LinearScaling(2, 1))
-                        .setRule(LinearDataRule(2, 3))
-                        .setSampleType(SampleType::Float64)
-                        //.setStructFields()
-                        .setTickResolution(Ratio(1, 666))
-                        .setUnit(unit)
-                        .setValueRange(Range(-35, 35))
-                        .build();
-    auto dataDescProp = PropertyBuilder("TestDataDescProp").setDefaultValue(dataDesc).setValueType(CoreType::ctStruct).build();
+    auto dims = daq::List<daq::IDimension>();
+    dims.pushBack(Dimension(daq::LinearDimensionRule(1, 2, 3), unit, daq::String("TestDimensionName")));
+    auto dataDesc = daq::DataDescriptorBuilder()
+                    .setDimensions(dims)
+                    //.setMetadata()
+                    .setName("TestDataDescriptor")
+                    .setOrigin("1970")
+                    //.setPostScaling(LinearScaling(2, 1))
+                    .setRule(daq::LinearDataRule(2, 3))
+                    .setSampleType(daq::SampleType::Float64)
+                    //.setStructFields()
+                    .setTickResolution(daq::Ratio(1, 666))
+                    .setUnit(unit)
+                        .setValueRange(daq::streaming_protocol::Range(20))
+                    .build();
+    auto dataDescProp = daq::PropertyBuilder("TestDataDescProp").setDefaultValue(dataDesc).setValueType(daq::CoreType::ctStruct).build();
     instance.addProperty(dataDescProp);
     // Add additional Data Descriptor Property (Post Scaling)
-    auto altDataDesc = DataDescriptorBuilder().setPostScaling(LinearScaling(2, 1)).setSampleType(SampleType::Float64).build();
-    auto altDataDescProp = PropertyBuilder("TestAltDataDescProp").setDefaultValue(altDataDesc).setValueType(CoreType::ctStruct).build();
+    auto altDataDesc =
+        daq::DataDescriptorBuilder().setPostScaling(daq::LinearScaling(2, 1)).setSampleType(daq::SampleType::Float64).build();
+    auto altDataDescProp =
+        daq::PropertyBuilder("TestAltDataDescProp").setDefaultValue(altDataDesc).setValueType(daq::CoreType::ctStruct).build();
     instance.addProperty(altDataDescProp);
     // Add Device Domain Property
-    auto deviceDomain = DeviceDomain(Ratio(3, 4), String("1997"), unit);
-    auto deviceDomainProp = PropertyBuilder("TestDeviceDomainProp").setDefaultValue(deviceDomain).setValueType(CoreType::ctStruct).build();
+    auto deviceDomain = daq::DeviceDomain(daq::Ratio(3, 4), daq::String("1997"), unit);
+    auto deviceDomainProp =
+        daq::PropertyBuilder("TestDeviceDomainProp").setDefaultValue(deviceDomain).setValueType(daq::CoreType::ctStruct).build();
     instance.addProperty(deviceDomainProp);
 
     // Create an empty file named "ready" to let regression test suite know
@@ -108,7 +113,7 @@ int main (int /*argc*/, const char* /*argv*/[])
     // we can then gracefully shut down the simulator
     while (std::filesystem::exists("ready"))
     {
-        std::this_thread::sleep_for(100ms);
+        std::this_thread::sleep_for(100);
     }
 
     return 0;

83fab3574a2a09e2e8b1c6dd86d362f40bf6f4d6
Aljaž <aljaz.francic@gmail.com>
2024-11-25 12:49:46 +0100

    Fix header

--- core/opendaq/modulemanager/include/opendaq/module_info_impl.h
+++ core/opendaq/modulemanager/include/opendaq/module_info_impl.h
@@ -1,7 +1,7 @@
 /*
  * Copyright 2022-2024 openDAQ d.o.o.
  *
- * Licensed under the Apache License, Module 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *

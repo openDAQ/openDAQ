bab87bf527ce5e054a2cfd965a87d1f081857af9
Aljaž <aljaz.francic@gmail.com>
2024-11-25 15:47:12 +0100

    Remove `defineIComponentType` from `py_core_objects.h`

--- bindings/python/core_objects/include/py_core_objects/py_core_objects.h
+++ bindings/python/core_objects/include/py_core_objects/py_core_objects.h
@@ -59,7 +59,6 @@ void defineIPropertyValueEventArgs(pybind11::module_ m, PyDaqIntf<daq::IProperty
 void defineIValidator(pybind11::module_ m, PyDaqIntf<daq::IValidator, daq::IBaseObject> cls);
 void defineIUnit(pybind11::module_ m, PyDaqIntf<daq::IUnit, daq::IBaseObject> cls);
 void defineIUnitBuilder(pybind11::module_ m, PyDaqIntf<daq::IUnitBuilder, daq::IBaseObject> cls);
-void defineIComponentType(pybind11::module_ m, PyDaqIntf<daq::IComponentType, daq::IBaseObject> cls);
 void defineIUser(pybind11::module_ m, PyDaqIntf<daq::IUser, daq::IBaseObject> cls);
 void defineIAuthenticationProvider(pybind11::module_ m, PyDaqIntf<daq::IAuthenticationProvider, daq::IBaseObject> cls);
 void defineIPermissionsBuilder(pybind11::module_ m, PyDaqIntf<daq::IPermissionsBuilder, daq::IBaseObject> cls);

66323009ac0f276b7fe128f8879cc614bad9249a
Jaka Mohorko <jaka.mohorko@dewesoft.com>
2024-12-16 10:28:32 +0100

    Remove root device on instance destruction

--- core/opendaq/opendaq/src/instance_impl.cpp
+++ core/opendaq/opendaq/src/instance_impl.cpp
@@ -64,6 +64,7 @@ InstanceImpl::InstanceImpl(IInstanceBuilder* instanceBuilder)
 InstanceImpl::~InstanceImpl()
 {
     stopAndRemoveServers();
+    rootDevice.remove();
     rootDevice.release();
 }
 
@@ -132,8 +133,11 @@ static ContextPtr ContextFromInstanceBuilder(IInstanceBuilder* instanceBuilder)
     return Context(scheduler, logger, typeManager, moduleManager, authenticationProvider, options, discoveryServers);
 }
 
-void InstanceImpl::stopAndRemoveServers()
+void InstanceImpl::stopAndRemoveServers() const
 {
+    if (rootDevice.isRemoved())
+        return;
+
     for (const auto& server : rootDevice.getServers())
     {
         server.stop();

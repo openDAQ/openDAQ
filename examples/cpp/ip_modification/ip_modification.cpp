c8df0b05140e4b6524c0ee8953a3bf0ffeb627bd
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2025-01-20 10:29:42 +0100

    Search device simulator by name

--- examples/cpp/ip_modification/ip_modification.cpp
+++ examples/cpp/ip_modification/ip_modification.cpp
@@ -11,42 +11,41 @@ int main(int /*argc*/, const char* /*argv*/[])
     using namespace daq;
     auto availableDevices = instance.getAvailableDevices();
 
-    for (const auto& devInfo : availableDevices)
+    for (const auto& deviceInfo : availableDevices)
     {
-        if (devInfo.getConnectionString().toStdString().find("daq://openDAQ_") != std::string::npos)
+        if (deviceInfo.getName() == "Reference device simulator")
         {
-            std::cout << "Found simulator device: " << devInfo.getConnectionString() << std::endl;
-            std::cout << "Count of network interfaces: " << devInfo.getNetworkInterfaces().getCount() << std::endl;
-            if (devInfo.getNetworkInterfaces().hasKey("enp0s3"))
+            std::cout << "Found simulator device: " << deviceInfo.getConnectionString() << std::endl;
+
+            const auto dhcp4 = False;
+            const auto addresses4 = List<IString>("192.168.56.155/24");
+            const auto gateway4 = String("192.168.56.1");
+            const auto dhcp6 = True;
+            const auto addresses6 = List<IString>();
+            const auto gateway6 = String("");
+
+            auto ipConfig = deviceInfo.getNetworkInterface("enp0s3").createDefaultConfiguration();
+            ipConfig.setPropertyValue("dhcp4", dhcp4);
+            ipConfig.setPropertyValue("addresses4", addresses4);
+            ipConfig.setPropertyValue("gateway4", gateway4);
+            ipConfig.setPropertyValue("dhcp6", dhcp6);
+            ipConfig.setPropertyValue("addresses6", addresses6);
+            ipConfig.setPropertyValue("gateway6", gateway6);
+
+            try
             {
-                const auto dhcp4 = True;
-                const auto addresses4 = List<IString>();
-                const auto gateway4 = String("");
-                const auto dhcp6 = True;
-                const auto addresses6 = List<IString>();
-                const auto gateway6 = String("");
-
-                auto ipConfig = devInfo.getNetworkInterface("enp0s3").createDefaultConfiguration();
-                ipConfig.setPropertyValue("dhcp4", dhcp4);
-                ipConfig.setPropertyValue("addresses4", addresses4);
-                ipConfig.setPropertyValue("gateway4", gateway4);
-                ipConfig.setPropertyValue("dhcp6", dhcp6);
-                ipConfig.setPropertyValue("addresses6", addresses6);
-                ipConfig.setPropertyValue("gateway6", gateway6);
-
-                try
-                {
-                    std::cout << "Device " << devInfo.getConnectionString() << " submitting new IP config..." << std::endl;
-                    devInfo.getNetworkInterface("enp0s3").submitConfiguration(ipConfig);
-                    std::cout << "success!" << std::endl;
-                }
-                catch (const std::exception& e)
-                {
-                    std::cout << "Error: " << e.what() << std::endl;
-                }
+                std::cout << "Device " << deviceInfo.getConnectionString() << " submitting new IP config..." << std::endl;
+                deviceInfo.getNetworkInterface("enp0s3").submitConfiguration(ipConfig);
+                std::cout << "Done!" << std::endl;
             }
+            catch (const std::exception& e)
+            {
+                std::cout << "Error: " << e.what() << std::endl;
+            }
+            return 0;
         }
     }
 
+    std::cout << "No simulator device found" << std::endl;
     return 0;
 }

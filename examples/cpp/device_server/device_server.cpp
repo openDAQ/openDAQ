07cf57163e39cb99ad1b35a0d204f89f875410c1
Aljaž <aljaz.francic@gmail.com>
2025-01-30 14:21:37 +0100

    Add C++ server

--- examples/cpp/device_server/device_server.cpp
+++ examples/cpp/device_server/device_server.cpp
@@ -1,8 +1,3 @@
-/**
- * Creates a simulated reference device and sets it as the root device.
- * An openDAQ server and a web-socket streaming server are started on the aplication.
- */
-
 #include <iostream>
 #include <opendaq/opendaq.h>
 
@@ -10,16 +5,14 @@ using namespace daq;
 
 int main(int /*argc*/, const char* /*argv*/[])
 {
-    // Create an openDAQ instance, loading modules at MODULE_PATH
-    const InstancePtr instance = Instance(MODULE_PATH);
-
-    // Add a reference device as root device
+    const InstancePtr instance = Instance("");
     instance.setRootDevice("daqref://device0");
-    
-    // Start streaming and openDAQ OpcUa servers
-    instance.addStandardServers();
-    
-    std::cout << "Press \"enter\" to exit the application..." << std::endl;
+    auto dummy = instance.addStandardServers();
+    instance.addProperty(ListProperty("listString", List<IString>()));
+    instance.addProperty(ListProperty("listBase", List<IBaseObject>()));
+    instance.addProperty(ListProperty("listInt", List<IInteger>()));
+    instance.addProperty(DictProperty("dict", Dict<IBaseObject, IBaseObject>()));
+    instance.addProperty(DictProperty("dictString", Dict<IString, IString>()));
     std::cin.get();
     return 0;
 }

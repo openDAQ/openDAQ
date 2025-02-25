f5d38aa7cce30b987c13fe238c5feb8a5d4e8d05
David Norris <david.norris@opendaq.com>
2025-02-25 09:39:15 +0100

    new WebSocket streaming server module

---
+++ modules/new_websocket_streaming_server_module/tests/test_app.cpp
@@ -0,0 +1,22 @@
+#include <opendaq/module_manager_init.h>
+#include <opendaq/opendaq_init.h>
+#include <coreobjects/util.h>
+#include <coretypes/stringobject_factory.h>
+#include <testutils/bb_memcheck_listener.h>
+#include <testutils/testutils.h>
+
+int main(int argc, char** args)
+{
+    daq::daqInitializeCoreObjectsTesting();
+    daqInitModuleManagerLibrary();
+    daqInitOpenDaqLibrary();
+
+    testing::InitGoogleTest(&argc, args);
+
+    testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+    listeners.Append(new DaqMemCheckListener());
+
+    auto res = RUN_ALL_TESTS();
+
+    return res;
+}

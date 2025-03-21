b43a770f5676f0f130f0f452286fbf9ad2f3f588
tguimaraesba <133114582+tguimaraesba@users.noreply.github.com>
2025-03-21 07:37:16 +0100

    [openDAQ] Fix ctutils initialization yielding warning as error (#739)

--- core/coretypes/include/coretypes/ctutils.h
+++ core/coretypes/include/coretypes/ctutils.h
@@ -140,7 +140,7 @@ inline ErrCode errorFromException(const std::exception& e, IBaseObject* source =
 template <typename... Params>
 void setErrorInfoWithSource(IBaseObject* source, const std::string& message, Params... params)
 {
-    IErrorInfo* errorInfo;
+    IErrorInfo* errorInfo = nullptr;
     auto err = createErrorInfoObjectWithSource(&errorInfo, source, message, params...);
     if (OPENDAQ_FAILED(err))
         return;

7976c3cc6eda9b567603f9eeb86390a7e02b2dba
NikolaiShipilov <127689162+NikolaiShipilov@users.noreply.github.com>
2025-04-02 01:58:55 -0700

    Fix chrono includes and CMake 4.0.0 compatibility for 3.10 (#749)

--- modules/ref_device_module/src/ref_channel_impl.cpp
+++ modules/ref_device_module/src/ref_channel_impl.cpp
@@ -13,7 +13,7 @@
 #include <opendaq/custom_log.h>
 #include <coreobjects/property_object_protected_ptr.h>
 #include <coreobjects/argument_info_factory.h>
-
+#include <chrono>
 
 #define PI 3.141592653589793
 

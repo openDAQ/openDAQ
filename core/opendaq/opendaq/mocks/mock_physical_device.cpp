c8b596c682d4f0c73632ba5c6e0962d8e6d85b34
Denis Erokhin <denis.erokhin@opendaq.com>
2025-04-24 14:19:07 +0200

    init

--- core/opendaq/opendaq/mocks/mock_physical_device.cpp
+++ core/opendaq/opendaq/mocks/mock_physical_device.cpp
@@ -65,8 +65,15 @@ inline MockPhysicalDeviceImpl::MockPhysicalDeviceImpl(const ContextPtr& ctx,
     registerProperties();
     registerNetworkConfigProperties();
 
-    const PropertyObjectPtr thisPtr = this->borrowPtr<PropertyObjectPtr>();
-    thisPtr.addProperty(StringProperty("TestProperty", "Test").detach());
+    this->addProperty(StringProperty("TestProperty", "Test"));
+
+    auto structFields = daq::Dict<daq::IString, daq::IBaseObject>(
+    {
+        {"value1", 0.0},
+        {"value2", 0.0}
+    });
+    this->addProperty(daq::StructProperty("DeviceStructure", daq::Struct("TestMockStructure", structFields, ctx.getTypeManager())));
+
     this->tags.add("phys_device");
 
     this->setDeviceDomain(DeviceDomain(Ratio(123, 456), "Origin", Unit("UnitSymbol", 987, "UnitName", "UnitQuantity")));
--- core/opendaq/opendaq/tests/test_instance.cpp
+++ core/opendaq/opendaq/tests/test_instance.cpp
@@ -947,6 +947,29 @@ TEST_F(InstanceTest, DISABLED_SaveLoadServers)
     ASSERT_EQ(servers[0].getId(), serverId);
 }
 
+TEST_F(InstanceTest, SaveLoadDeviceStruct)
+{
+    StringPtr config;
+    StringPtr serverId;
+    {
+        auto instance = test_helpers::setupInstance();
+        instance.addDevice("daqmock://phys_device");
+
+        auto device = instance.getDevices()[0];
+        StructPtr deviceStruct = device.getPropertyValue("DeviceStructure");
+        deviceStruct = StructBuilder(deviceStruct).set("value1", 5e-7);
+        device.setPropertyValue("DeviceStructure", deviceStruct);
+
+        config = instance.saveConfiguration();
+    }
+
+    auto instance = Instance();
+    instance.loadConfiguration(config);
+    auto device = instance.getDevices()[0];
+    StructPtr deviceStruct = device.getPropertyValue("DeviceStructure");
+    ASSERT_EQ(deviceStruct.get("value1"), 5e-7);
+}
+
 TEST_F(InstanceTest, TestRemoved1)
 {
     auto instance = test_helpers::setupInstance();

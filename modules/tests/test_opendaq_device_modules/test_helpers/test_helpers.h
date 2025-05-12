d5df3806df2c6ef2f72f6d04b203b87e517573eb
Jaka Mohorko <jaka.mohorko@dewesoft.com>
2025-05-12 12:54:11 +0200

    Increase subscription ack timeout

--- modules/tests/test_opendaq_device_modules/test_helpers/test_helpers.h
+++ modules/tests/test_opendaq_device_modules/test_helpers/test_helpers.h
@@ -101,7 +101,7 @@ namespace test_helpers
     [[maybe_unused]]
     static bool waitForAcknowledgement(
         std::future<StringPtr>& acknowledgementFuture,
-        std::chrono::seconds timeout = std::chrono::seconds(5))
+        std::chrono::seconds timeout = std::chrono::seconds(30))
     {
         return acknowledgementFuture.wait_for(timeout) == std::future_status::ready;
     }

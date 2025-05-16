291007e483e7fe00c05a414a6ee790d46dc1ce71
DenisE <deniserokhinme@gmail.com>
2025-05-16 14:21:56 +0200

    fix sync component

--- core/opendaq/synchronization/include/opendaq/sync_component_impl.h
+++ core/opendaq/synchronization/include/opendaq/sync_component_impl.h
@@ -158,7 +158,10 @@ ErrCode GenericSyncComponentImpl<MainInterface, Interfaces...>::checkClassNameIs
 
     TypePtr type;
     ErrCode errCode = manager->getType(className, &type);
-    OPENDAQ_RETURN_IF_FAILED(errCode, fmt::format("Interface '{}' is not registered in type manager.", className));
+    if (OPENDAQ_FAILED(errCode))
+    {
+        return DAQ_EXTEND_ERROR_INFO(OPENDAQ_ERR_INVALID_ARGUMENT, fmt::format("Interface '{}' is not registered in type manager.", className));
+    }
 
     if (auto objectClass = type.asPtrOrNull<IPropertyObjectClass>(true); objectClass.assigned())
     {

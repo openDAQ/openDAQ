6eca043d293c149315e6f1f82a2642fe75940c3b
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2025-05-16 11:57:31 +0200

    Add property and component specific search filter factories to dotnet bindings

---
+++ bindings/dotnet/openDAQ.Net/openDAQ.Net/core/coreobjects/SearchFilter.cs
@@ -0,0 +1,155 @@
+using Daq.Core.Types;
+
+namespace Daq.Core.Objects;
+
+#region Class Factory
+
+// Factory functions of the &apos;CoreObjects&apos; library.
+public static partial class CoreObjectsFactory
+{
+    //ErrorCode createVisiblePropertyFilter(daq.ISearchFilter** obj); cdecl;
+    [DllImport(CoreObjectsDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createVisiblePropertyFilter(out IntPtr obj);
+
+    /// <summary>Creates a search filter that accepts only visible properties.</summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    public static ErrorCode CreateVisiblePropertyFilter(out SearchFilter obj)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createVisiblePropertyFilter(out objPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>Creates a search filter that accepts only visible properties.</summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    public static SearchFilter CreateVisiblePropertyFilter()
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createVisiblePropertyFilter(out objPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+
+
+    //ErrorCode createReadOnlyPropertyFilter(daq.ISearchFilter** obj); cdecl;
+    [DllImport(CoreObjectsDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createReadOnlyPropertyFilter(out IntPtr obj);
+
+    /// <summary>Creates a search filter that accepts only read-only properties.</summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    public static ErrorCode CreateReadOnlyPropertyFilter(out SearchFilter obj)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createReadOnlyPropertyFilter(out objPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>Creates a search filter that accepts only read-only properties.</summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    public static SearchFilter CreateReadOnlyPropertyFilter()
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createReadOnlyPropertyFilter(out objPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+
+    //ErrorCode createNamePropertyFilter(daq.ISearchFilter** obj, daq.IString* regex); cdecl;
+    [DllImport(CoreObjectsDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createNamePropertyFilter(out IntPtr obj, IntPtr regex);
+
+    /// <summary>Creates a search filter that accepts properties whose names match the specified pattern.</summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    /// <param name="regex">A regular expression pattern used to match property names.</param>
+    public static ErrorCode CreateNamePropertyFilter(out SearchFilter obj, string regex)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        using var regexPtr = (StringObject)regex;
+
+        //call native function
+        ErrorCode errorCode = createNamePropertyFilter(out objPtr, regexPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>Creates a search filter that accepts properties whose names match the specified pattern.</summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    /// <param name="name">A regular expression pattern used to match property names.</param>
+    public static SearchFilter CreateNamePropertyFilter(string regex)
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        using var regexPtr = (StringObject)regex;
+
+        //call native function
+        ErrorCode errorCode = createNamePropertyFilter(out objPtr, regexPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+}
+
+#endregion Class Factory
---
+++ bindings/dotnet/openDAQ.Net/openDAQ.Net/core/opendaq/component/SearchFilter.cs
@@ -0,0 +1,243 @@
+using Daq.Core.Types;
+using Daq.Core.Objects;
+
+namespace Daq.Core.OpenDAQ;
+
+#region Class Factory
+
+// Factory functions of the &apos;OpenDAQ&apos; library.
+public static partial class OpenDAQFactory
+{
+    //ErrorCode createVisibleSearchFilter(daq.ISearchFilter** obj); cdecl;
+    [DllImport(OpenDAQDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createVisibleSearchFilter(out IntPtr obj);
+
+    /// <summary>
+    /// Creates a search filter that accepts only visible components. &quot;Visit children&quot; returns <c>true</c>
+    /// only if the component being evaluated is visible.
+    /// </summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    public static ErrorCode CreateVisibleSearchFilter(out SearchFilter obj)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createVisibleSearchFilter(out objPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>
+    /// Creates a search filter that accepts only visible components. &quot;Visit children&quot; returns <c>true</c>
+    /// only if the component being evaluated is visible.
+    /// </summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    public static SearchFilter CreateVisibleSearchFilter()
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //call native function
+        ErrorCode errorCode = createVisibleSearchFilter(out objPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+
+
+    //ErrorCode createRequiredTagsSearchFilter(daq.ISearchFilter** obj, daq.IList* requiredTags); cdecl;
+    [DllImport(OpenDAQDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createRequiredTagsSearchFilter(out IntPtr obj, IntPtr requiredTags);
+
+    /// <summary>
+    /// Creates a search filter that accepts components that have all the required tags. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    /// <param name="requiredTags">A list of strings containing the tags that a component must have to be accepted.</param>
+    public static ErrorCode CreateRequiredTagsSearchFilter(out SearchFilter obj, IListObject<BaseObject> requiredTags)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        var requiredTagsPtr = (ListObject<BaseObject>)requiredTags;
+
+        //call native function
+        ErrorCode errorCode = createRequiredTagsSearchFilter(out objPtr, requiredTagsPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>
+    /// Creates a search filter that accepts components that have all the required tags. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    /// <param name="requiredTags">A list of strings containing the tags that a component must have to be accepted.</param>
+    public static SearchFilter CreateRequiredTagsSearchFilter(IListObject<BaseObject> requiredTags)
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        var requiredTagsPtr = (ListObject<BaseObject>)requiredTags;
+
+        //call native function
+        ErrorCode errorCode = createRequiredTagsSearchFilter(out objPtr, requiredTagsPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+
+
+    //ErrorCode createExcludedTagsSearchFilter(daq.ISearchFilter** obj, daq.IList* excludedTags); cdecl;
+    [DllImport(OpenDAQDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createExcludedTagsSearchFilter(out IntPtr obj, IntPtr excludedTags);
+
+    /// <summary>
+    /// Creates a search filter that accepts components that do not have any of the excluded tags. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    /// <param name="excludedTags">A list of strings containing the tags that a component must not have to be accepted.</param>
+    public static ErrorCode CreateExcludedTagsSearchFilter(out SearchFilter obj, IListObject<BaseObject> excludedTags)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        var excludedTagsPtr = (ListObject<BaseObject>)excludedTags;
+
+        //call native function
+        ErrorCode errorCode = createExcludedTagsSearchFilter(out objPtr, excludedTagsPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>
+    /// Creates a search filter that accepts components that do not have any of the excluded tags. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    /// <param name="excludedTags">A list of strings containing the tags that a component must not have to be accepted.</param>
+    public static SearchFilter CreateExcludedTagsSearchFilter(IListObject<BaseObject> excludedTags)
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        var excludedTagsPtr = (ListObject<BaseObject>)excludedTags;
+
+        //call native function
+        ErrorCode errorCode = createExcludedTagsSearchFilter(out objPtr, excludedTagsPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+
+
+    //ErrorCode createLocalIdSearchFilter(daq.ISearchFilter** obj, daq.IString* localId); cdecl;
+    [DllImport(OpenDAQDllInfo.FileName, CallingConvention = CallingConvention.Cdecl)]
+    private static extern ErrorCode createLocalIdSearchFilter(out IntPtr obj, IntPtr localId);
+
+    /// <summary>
+    /// Creates a search filter that accepts components with the specified local ID. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <param name="obj">The &apos;SearchFilter&apos; object.</param>
+    /// <param name="localId">The local ID of the accepted components.</param>
+    public static ErrorCode CreateLocalIdSearchFilter(out SearchFilter obj, string localId)
+    {
+        //initialize output argument
+        obj = default;
+
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        using var localIdPtr = (StringObject)localId;
+
+        //call native function
+        ErrorCode errorCode = createLocalIdSearchFilter(out objPtr, localIdPtr);
+
+        if (Result.Succeeded(errorCode))
+        {
+            //create object
+            obj = new SearchFilter(objPtr, incrementReference: false);
+        }
+
+        return errorCode;
+    }
+
+    /// <summary>
+    /// Creates a search filter that accepts components with the specified local ID. &quot;Visit children&quot;
+    /// always returns <c>true</c>.
+    /// </summary>
+    /// <returns>The &apos;SearchFilter&apos; object.</returns>
+    /// <param name="localId">The local ID of the accepted components.</param>
+    public static SearchFilter CreateLocalIdSearchFilter(string localId)
+    {
+        //native output argument
+        IntPtr objPtr;
+
+        //cast .NET argument to SDK object
+        using var localIdPtr = (StringObject)localId;
+
+        //call native function
+        ErrorCode errorCode = createLocalIdSearchFilter(out objPtr, localIdPtr);
+
+        if (Result.Failed(errorCode))
+        {
+            throw new OpenDaqException(errorCode);
+        }
+
+        //create and return object
+        return new SearchFilter(objPtr, incrementReference: false);
+    }
+}
+
+#endregion Class Factory

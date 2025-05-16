108f558ba4c1146c6a80900ca7a68c1950b9c37e
Aljaž <aljaz.francic@gmail.com>
2025-05-16 14:27:09 +0200

    Revert `quick_start_empty.cpp`

--- examples/cpp/quick_start/quick_start_empty.cpp
+++ examples/cpp/quick_start/quick_start_empty.cpp
@@ -3,30 +3,17 @@
  * example can be found in app_quick_start_full.cpp
  */
 
+#include <opendaq/opendaq.h>
 #include <chrono>
 #include <iostream>
 #include <thread>
-#include <opendaq/opendaq.h>
 
 int main(int /*argc*/, const char* /*argv*/[])
 {
     // Create an Instance, loading modules at MODULE_PATH
     const daq::InstancePtr instance = daq::Instance(MODULE_PATH);
 
-    auto device = instance.addDevice("daqref://device0");
-
-    auto modes = device.getAvailableOperationModes();
-    std::cout << "Available operation modes:\n";
-    for (const auto& mode : modes) {
-        std::cout << "  " << mode << "\n";
-    }
-
-    auto oldMode = device.getOperationMode();
-    device.setOperationMode(daq::OperationModeType::Idle);
-    auto newMode = device.getOperationMode();
-
-
-    std::cout << "Press \"enter\" to exit the application...\n";
+    std::cout << "Press \"enter\" to exit the application..." << std::endl;
     std::cin.get();
     return 0;
-} 
+}

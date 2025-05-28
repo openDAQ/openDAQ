efff2511009964158ca02c5de9fce1e095793740
Denis Erokhin <149682271+denise-opendaq@users.noreply.github.com>
2025-05-28 13:30:44 +0200

    Fix registering the new property object class with the type manager on the native client. (#816)

--- core/coreobjects/include/coreobjects/property_object_class_impl.h
+++ core/coreobjects/include/coreobjects/property_object_class_impl.h
@@ -17,6 +17,7 @@
 #pragma once
 #include <coretypes/coretypes.h>
 #include <coreobjects/property_object_class.h>
+#include <coreobjects/property_object_class_internal.h>
 #include <coreobjects/property_object_class_builder.h>
 #include <coreobjects/property_object_class_builder_ptr.h>
 #include <coreobjects/property_ptr.h>
@@ -31,18 +32,20 @@ struct ISerializedObject;
 
 using PropertyOrderedMap = tsl::ordered_map<StringPtr, PropertyPtr, StringHash, StringEqualTo>;
 
-class PropertyObjectClassImpl : public ImplementationOf<IPropertyObjectClass, ISerializable>
+class PropertyObjectClassImpl : public ImplementationOf<IPropertyObjectClass, IPropertyObjectClassInternal, ISerializable>
 {
 public:
     explicit PropertyObjectClassImpl(IPropertyObjectClassBuilder* builder);
 
     // IPropertyObjectClass
-
     ErrCode INTERFACE_FUNC getName(IString** typeName) override;
     ErrCode INTERFACE_FUNC getParentName(IString** parentName) override;
     ErrCode INTERFACE_FUNC getProperty(IString* propertyName, IProperty** property) override;
     ErrCode INTERFACE_FUNC hasProperty(IString* propertyName, Bool* hasProperty) override;
     ErrCode INTERFACE_FUNC getProperties(Bool includeInherited, IList** properties) override;
+
+    // IPropertyObjectClassInternal
+    ErrCode INTERFACE_FUNC clone(IPropertyObjectClass** cloned, ITypeManager* typeManager = nullptr) override;
 
     ErrCode INTERFACE_FUNC serialize(ISerializer* serializer) override;
     ErrCode INTERFACE_FUNC getSerializeId(ConstCharPtr* id) const override;

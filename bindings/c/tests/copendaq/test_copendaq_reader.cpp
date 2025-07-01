33acff24d4df175210f24e429e0d871926375377
Denis Erokhin <denis.erokhin@opendaq.com>
2025-07-01 17:52:47 +0200

    wrong branch

--- bindings/c/tests/copendaq/test_copendaq_reader.cpp
+++ bindings/c/tests/copendaq/test_copendaq_reader.cpp
@@ -106,7 +106,7 @@ protected:
         daqTypeManager_createTypeManager(&typeManager);
 
         daqScheduler* scheduler = nullptr;
-        daqScheduler_createScheduler(&scheduler, logger);
+        daqScheduler_createScheduler(&scheduler, logger, 1);
 
         daqDict *options = nullptr, *discoveryServers = nullptr;
         daqDict_createDict(&options);

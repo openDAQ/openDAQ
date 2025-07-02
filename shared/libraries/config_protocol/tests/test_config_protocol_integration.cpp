ad79683e54e9f4fe993e9b268a1439083d168856
NikolaiShipilov <127689162+NikolaiShipilov@users.noreply.github.com>
2025-07-02 02:24:34 -0700

    Allow parentless signals connection within client-to-device streaming (#851)
    * Allow outside-of-tree signals within C2D streaming
    * Align signal acceptance logic with C2D streaming

--- shared/libraries/config_protocol/tests/test_config_protocol_integration.cpp
+++ shared/libraries/config_protocol/tests/test_config_protocol_integration.cpp
@@ -726,16 +726,18 @@ TEST_F(ConfigProtocolIntegrationTest, AcceptsSignal)
                         clientDevice.getDevices()[0].getFunctionBlocks()[0].getInputPorts()[0].acceptsSignal(clientSignal));
     ASSERT_TRUE(clientAcceptsClientSignal);
 
-    auto serverSignal = serverDevice.getSignals()[0];
-    daq::Bool clientAcceptsServerSignal = True;
-    ASSERT_NO_THROW(clientAcceptsServerSignal =
-                        clientDevice.getDevices()[0].getFunctionBlocks()[0].getInputPorts()[0].acceptsSignal(serverSignal));
-    ASSERT_FALSE(clientAcceptsServerSignal);
+    auto parentlessSignal = Signal(clientSignal.getContext(), nullptr, "test");
+    daq::Bool clientAcceptsParentlessSignal = False;
+    ASSERT_NO_THROW(clientAcceptsParentlessSignal =
+                        clientDevice.getDevices()[0].getFunctionBlocks()[0].getInputPorts()[0].acceptsSignal(parentlessSignal));
+    ASSERT_TRUE(clientAcceptsParentlessSignal);
 
-    auto falseSignal = Signal(clientSignal.getContext(), nullptr, "test");
-    ASSERT_THROW_MSG(clientDevice.getDevices()[0].getFunctionBlocks()[0].getInputPorts()[0].acceptsSignal(falseSignal),
-                     NativeClientCallNotAvailableException,
-                     "Signal is not from the same component tree");
+    // such connection likely creates unsafe loopback with undefined behavior, but signal is still accepted
+    auto serverSignal = serverDevice.getSignals()[0];
+    daq::Bool clientAcceptsServerSignal = False;
+    ASSERT_NO_THROW(clientAcceptsServerSignal =
+                    clientDevice.getDevices()[0].getFunctionBlocks()[0].getInputPorts()[0].acceptsSignal(serverSignal));
+    ASSERT_TRUE(clientAcceptsServerSignal);
 }
 
 TEST_F(ConfigProtocolIntegrationTest, GetAvailableDevices)

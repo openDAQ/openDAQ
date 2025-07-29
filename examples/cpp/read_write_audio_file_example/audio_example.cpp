deca0b3204ed6e314b19dec22efb1102927b0e32
JakobVokac <jakob.vokac@gmail.com>
2025-07-29 08:43:55 +0200

    implemented and integrated a WAV file reader (#845)

---
+++ examples/cpp/read_write_audio_file_example/audio_example.cpp
@@ -0,0 +1,35 @@
+/**
+ * Empty example
+ */
+
+#include <opendaq/opendaq.h>
+
+using namespace daq;
+
+
+int main(int /*argc*/, const char* /*argv*/[])
+{
+    // Set your wav file destination here
+    std::string filePath = "path/to/file";
+
+    // Create an Instance, loading modules at MODULE_PATH
+    const InstancePtr instance = Instance(MODULE_PATH);
+
+    daq::FunctionBlockPtr reader = instance.addFunctionBlock("AudioDeviceModuleWavReader");
+
+    daq::FunctionBlockPtr writerFb = instance.addFunctionBlock("AudioDeviceModuleWavWriter");
+    daq::RecorderPtr writerRe = writerFb;
+
+    writerFb.getInputPorts()[0].connect(reader.getSignals()[0]);
+
+    reader.setPropertyValue("FilePath", filePath);
+    writerFb.setPropertyValue("FileName", "output.wav");
+
+    reader.setPropertyValue("Reading", true);
+    writerRe.startRecording();
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    reader.setPropertyValue("Reading", false);
+    writerRe.stopRecording();
+}
---
+++ modules/audio_device_module/include/audio_device_module/wav_reader_fb_impl.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022-2025 openDAQ d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <audio_device_module/common.h>
+#include <miniaudio/miniaudio.h>
+#include <opendaq/data_packet_ptr.h>
+#include <opendaq/function_block_impl.h>
+#include <opendaq/input_port_config_ptr.h>
+#include <opendaq/stream_reader_ptr.h>
+
+
+BEGIN_NAMESPACE_AUDIO_DEVICE_MODULE
+
+class WAVReaderFbImpl : public FunctionBlock
+{
+public:
+    explicit WAVReaderFbImpl(const ContextPtr& ctx, const ComponentPtr& parent, const StringPtr& localId);
+    ~WAVReaderFbImpl() override;
+
+    static FunctionBlockTypePtr CreateType();
+
+
+private:
+    bool initializeDecoder();
+    bool reinitializeDecoder();
+    void uninitializeDecoder();
+    bool decoderReady();
+    bool decoderReading();
+
+    void initProperties();
+    bool initializeSignal();
+
+    bool updateFilePath(const std::string& newPath);
+    void setRead(bool read);
+
+    void startRead();
+    void stopRead();
+
+    DataPacketPtr buildPacket(const void* data, size_t sampleCount);
+    void sendPacket(DataPacketPtr packet);
+
+    static void miniaudioDataCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount);
+
+public:
+    ma_decoder decoder;
+
+private:
+    std::string filePath;
+
+    ma_device device;
+
+    SignalConfigPtr timeSignal;
+    SignalConfigPtr outputSignal;
+
+    DataDescriptorPtr domainDescriptor;
+    DataDescriptorPtr dataDescriptor;
+
+    Int samplesCaptured;
+
+    bool decoderInitialized;
+    bool reading;
+    std::atomic<bool> framesAvailable;
+
+    LoggerPtr logger;
+    LoggerComponentPtr loggerComponent;
+};
+
+END_NAMESPACE_AUDIO_DEVICE_MODULE
+

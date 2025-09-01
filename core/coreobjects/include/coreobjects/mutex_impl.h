4568cd6c5686d7edda4d0ca3069e768db049a37e
Jaka Mohorko <jaka.mohorko@dewesoft.com>
2025-09-01 08:34:41 +0200

    Switch mutex impl to header-only

--- core/coreobjects/include/coreobjects/mutex_impl.h
+++ core/coreobjects/include/coreobjects/mutex_impl.h
@@ -17,6 +17,8 @@
 #pragma once
 #include <coretypes/intfs.h>
 #include <coreobjects/mutex.h>
+#include <fmt/format.h>
+#include <coretypes/impl.h>
 
 BEGIN_NAMESPACE_OPENDAQ
 
@@ -24,12 +26,56 @@ class MutexImpl : public ImplementationOf<IMutex>
 {
 public:
     explicit MutexImpl();
-
+    
     ErrCode INTERFACE_FUNC lock() override;
     ErrCode INTERFACE_FUNC tryLock(Bool* succeeded) override;
     ErrCode INTERFACE_FUNC unlock() override;
 
     std::mutex mutex;
 };
+
+inline MutexImpl::MutexImpl() = default;
+
+inline ErrCode MutexImpl::lock()
+{
+    try
+    {
+        this->mutex.lock();
+    }
+    catch (std::exception& e)
+    {
+        return DAQ_MAKE_ERROR_INFO(OPENDAQ_ERR_GENERALERROR, fmt::format("Failed to lock mutex: {}", e.what()));
+    }
+
+    return OPENDAQ_SUCCESS;
+}
+
+inline ErrCode MutexImpl::tryLock(Bool* succeeded)
+{
+    try
+    {
+        *succeeded = this->mutex.try_lock();
+    }
+    catch (std::exception& e)
+    {
+        return DAQ_MAKE_ERROR_INFO(OPENDAQ_ERR_GENERALERROR, fmt::format("Failed to try_lock mutex: {}", e.what()));
+    }
+
+    return OPENDAQ_SUCCESS;
+}
+
+inline ErrCode MutexImpl::unlock()
+{
+    try
+    {
+        this->mutex.unlock();
+    }
+    catch (std::exception& e)
+    {
+        return DAQ_MAKE_ERROR_INFO(OPENDAQ_ERR_GENERALERROR, fmt::format("Failed to unlock mutex: {}", e.what()));
+    }
+
+    return OPENDAQ_SUCCESS;
+}
 
 END_NAMESPACE_OPENDAQ

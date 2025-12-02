cc5e18d3eb103465533e57baf41ea5e6d11465b8
tomaz-cvetko <tomaz.cvetko@opendaq.com>
2025-12-02 11:16:23 +0100

    Add a FB with multiple inputs and zero output signals. (#982)
    * Add a FB with multiple inputs and zero output signals.
    
    * Write double samples to the same file (no header, no sample type flexibility).

---
+++ examples/modules/basic_csv_recorder_module/include/basic_csv_recorder_module/multi_csv_recorder_impl.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2022-2025 openDAQ d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fstream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <thread>
+
+#include <coretypes/filesystem.h>
+#include <opendaq/function_block_impl.h>
+#include <opendaq/opendaq.h>
+
+#include <basic_csv_recorder_module/common.h>
+#include <basic_csv_recorder_module/multi_csv_writer.h>
+
+BEGIN_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE
+
+/*!
+ * @brief A basic recorder function block which records data from its input signals into a CSV file.
+ */
+class MultiCsvRecorderImpl final : public FunctionBlockImpl<IFunctionBlock, IRecorder>
+{
+public:
+    struct SyncedSampleData
+    {
+        bool exit;
+        std::vector<double> data;
+    };
+
+    static constexpr const char* TYPE_ID = "MultiCsvRecorder";
+
+    struct Tags
+    {
+        static constexpr const char* RECORDER = "Recorder";
+    };
+
+    struct Props
+    {
+        static constexpr const char* PATH = "Path";
+    };
+
+    /*!
+     * @brief Creates a new function block.
+     * @param context The openDAQ context object.
+     * @param parent The component object which will contain this function block.
+     * @param localId The local identifier of this function block.
+     * @param config A property object containing configuration data for this function block.
+     */
+    MultiCsvRecorderImpl(const ContextPtr& context, const ComponentPtr& parent, const StringPtr& localId, const PropertyObjectPtr& config);
+
+    ~MultiCsvRecorderImpl() = default;
+
+    static FunctionBlockTypePtr createType();
+
+    ErrCode INTERFACE_FUNC startRecording() override;
+    ErrCode INTERFACE_FUNC stopRecording() override;
+    ErrCode INTERFACE_FUNC getIsRecording(Bool* isRecording) override;
+
+protected:
+    virtual void activeChanged() override;
+
+private:
+    void initProperties();
+
+    std::string getNextPortID() const;
+
+    bool updateInputPorts();
+    void updateReader();
+    void configure(const DataDescriptorPtr& domainDescriptor, const ListPtr<IDataDescriptor>& valueDescriptors);
+    void reconfigure();
+    void onPathChanged();
+
+    void onConnected(const InputPortPtr& inputPort) override;
+    void onDisconnected(const InputPortPtr& inputPort) override;
+    void onDataReceived();
+
+    std::vector<InputPortPtr> connectedPorts;
+    InputPortPtr disconnectedPort;
+
+    std::unordered_map<std::string, DataDescriptorPtr> cachedDescriptors;
+    DataDescriptorPtr sumDataDescriptor;
+    DataDescriptorPtr sumDomainDataDescriptor;
+
+    PacketReadyNotification notificationMode;
+    MultiReaderPtr reader;
+
+    bool recordingActive = false;
+    std::optional<fs::path> filePath = std::nullopt;
+    std::optional<MultiCsvWriter> writer = std::nullopt;
+};
+
+END_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE
---
+++ examples/modules/basic_csv_recorder_module/src/multi_csv_recorder_impl.cpp
@@ -0,0 +1,352 @@
+#include <functional>
+#include <memory>
+#include <set>
+#include <string>
+
+#include <coretypes/filesystem.h>
+#include <opendaq/event_packet_params.h>
+#include <opendaq/function_block_impl.h>
+#include <opendaq/opendaq.h>
+#include <opendaq/reader_config_ptr.h>
+#include <opendaq/reader_factory.h>
+
+#include <basic_csv_recorder_module/common.h>
+#include <basic_csv_recorder_module/multi_csv_recorder_impl.h>
+
+BEGIN_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE
+
+namespace
+{
+bool descriptorNotNull(const DataDescriptorPtr& descriptor)
+{
+    return descriptor.assigned() && descriptor != NullDataDescriptor();
+}
+
+void getDataDescriptors(const EventPacketPtr& eventPacket, DataDescriptorPtr& valueDesc, DataDescriptorPtr& domainDesc)
+{
+    if (eventPacket.getEventId() == event_packet_id::DATA_DESCRIPTOR_CHANGED)
+    {
+        valueDesc = eventPacket.getParameters().get(event_packet_param::DATA_DESCRIPTOR);
+        domainDesc = eventPacket.getParameters().get(event_packet_param::DOMAIN_DATA_DESCRIPTOR);
+    }
+}
+
+bool getDomainDescriptor(const EventPacketPtr& eventPacket, DataDescriptorPtr& domainDesc)
+{
+    if (eventPacket.getEventId() == event_packet_id::DATA_DESCRIPTOR_CHANGED)
+    {
+        domainDesc = eventPacket.getParameters().get(event_packet_param::DOMAIN_DATA_DESCRIPTOR);
+        return true;
+    }
+    return false;
+}
+}
+
+FunctionBlockTypePtr MultiCsvRecorderImpl::createType()
+{
+    auto config = PropertyObject();
+    config.addProperty(
+        SparseSelectionProperty("ReaderNotificationMode",
+                                Dict<IInteger, IString>({{static_cast<Int>(PacketReadyNotification::SameThread), "SameThread"},
+                                                         {static_cast<Int>(PacketReadyNotification::Scheduler), "Scheduler"}}),
+                                2));
+
+    return FunctionBlockType(TYPE_ID, "MultiCsvRecorder", "Multi Reader CSV recording functionality", config);
+}
+
+MultiCsvRecorderImpl::MultiCsvRecorderImpl(const ContextPtr& context,
+                                           const ComponentPtr& parent,
+                                           const StringPtr& localId,
+                                           const PropertyObjectPtr& config)
+    : FunctionBlockImpl<IFunctionBlock, IRecorder>(createType(), context, parent, localId, nullptr)
+{
+    initComponentStatus();
+    initProperties();
+
+    if (config.assigned())
+        notificationMode = static_cast<PacketReadyNotification>(config.getPropertyValue("ReaderNotificationMode"));
+    else
+        notificationMode = PacketReadyNotification::Scheduler;
+
+    updateInputPorts();
+}
+
+ErrCode MultiCsvRecorderImpl::startRecording()
+{
+    auto lock = getRecursiveConfigLock();
+    recordingActive = true;
+    reconfigure();
+
+    return OPENDAQ_SUCCESS;
+}
+
+ErrCode MultiCsvRecorderImpl::stopRecording()
+{
+    auto lock = getRecursiveConfigLock();
+    recordingActive = false;
+    reconfigure();
+
+    return OPENDAQ_SUCCESS;
+}
+
+ErrCode MultiCsvRecorderImpl::getIsRecording(Bool* isRecording)
+{
+    OPENDAQ_PARAM_NOT_NULL(isRecording);
+
+    auto lock = getRecursiveConfigLock();
+    *isRecording = recordingActive;
+
+    return OPENDAQ_SUCCESS;
+}
+
+void MultiCsvRecorderImpl::activeChanged()
+{
+    if (!active)
+        stopRecording();
+}
+
+void MultiCsvRecorderImpl::initProperties()
+{
+    this->tags.add(Tags::RECORDER);
+
+    objPtr.addProperty(StringProperty(Props::PATH, ""));
+    objPtr.getOnPropertyValueWrite(Props::PATH) += std::bind(&MultiCsvRecorderImpl::onPathChanged, this);
+}
+
+std::string MultiCsvRecorderImpl::getNextPortID() const
+{
+    int maxId = 0;
+    for (const auto& port : connectedPorts)
+    {
+        std::string portId = port.getLocalId();
+        auto pos = portId.find_last_of('_');
+        int curId = std::stoi(portId.substr(pos + 1));
+        maxId = curId > maxId ? curId : maxId;
+    }
+
+    return fmt::format("CsvRecorderPort_{}", maxId + 1);
+}
+
+bool MultiCsvRecorderImpl::updateInputPorts()
+{
+    bool rebuildReader = false;
+    if (disconnectedPort.assigned() && disconnectedPort.getConnection().assigned())
+    {
+        connectedPorts.emplace_back(disconnectedPort);
+        cachedDescriptors.insert(std::make_pair(disconnectedPort.getGlobalId(), NullDataDescriptor()));
+        disconnectedPort.release();
+        rebuildReader = true;
+    }
+
+    for (auto it = connectedPorts.begin(); it != connectedPorts.end();)
+    {
+        if (!it->getConnection().assigned())
+        {
+            cachedDescriptors.erase(it->getGlobalId().toStdString());
+            this->inputPorts.removeItem(*it);
+            it = connectedPorts.erase(it);
+            rebuildReader = true;
+        }
+        else
+        {
+            ++it;
+        }
+    }
+
+    if (!disconnectedPort.assigned())
+    {
+        std::string id = getNextPortID();
+        auto inputPort = createAndAddInputPort(id, notificationMode);
+        inputPort.setListener(this->thisPtr<InputPortNotificationsPtr>());
+        disconnectedPort = inputPort;
+    }
+
+    if (connectedPorts.empty())
+    {
+        setComponentStatusWithMessage(ComponentStatus::Warning, "No signals connected!");
+        reader = nullptr;
+        return false;
+    }
+
+    return rebuildReader;
+}
+
+// Reader must currently be rebuilt to add/remove input ports
+void MultiCsvRecorderImpl::updateReader()
+{
+    // Disposing the reader is necessary to release port ownership
+    reader.dispose();
+    auto builder = MultiReaderBuilder()
+                       .setDomainReadType(SampleType::Int64)
+                       .setValueReadType(SampleType::Float64)
+                       .setAllowDifferentSamplingRates(false)
+                       .setInputPortNotificationMethod(notificationMode);
+
+    for (const auto& port : connectedPorts)
+        builder.addInputPort(port);
+
+    reader = builder.build();
+
+    // TODO: Check if reader is valid - different sample rates or sth
+
+    reader.setExternalListener(this->thisPtr<InputPortNotificationsPtr>());
+    auto thisWeakRef = this->template getWeakRefInternal<IFunctionBlock>();
+    reader.setOnDataAvailable(
+        [this, thisWeakRef = std::move(thisWeakRef)]
+        {
+            const auto thisFb = thisWeakRef.getRef();
+            if (thisFb.assigned())
+                this->onDataReceived();
+        });
+}
+
+void MultiCsvRecorderImpl::configure(const DataDescriptorPtr& domainDescriptor, const ListPtr<IDataDescriptor>& valueDescriptors)
+{
+    // TODO: These value descripters need to be compiled into a header
+    try
+    {
+        if (!domainDescriptor.assigned() || domainDescriptor == NullDataDescriptor())
+        {
+            throw std::runtime_error("Input domain descriptor is not set");
+        }
+
+        if (valueDescriptors.getCount() != connectedPorts.size())
+        {
+            throw std::runtime_error("Missing input value descriptors!");
+        }
+
+        sumDomainDataDescriptor = domainDescriptor;
+
+        setComponentStatus(ComponentStatus::Ok);
+        reader.setActive(True);
+    }
+    catch (const std::exception& e)
+    {
+        setComponentStatusWithMessage(ComponentStatus::Warning, fmt::format("Failed to set descriptor for power signal: {}", e.what()));
+        reader.setActive(False);
+    }
+
+    if (this->statusContainer.getStatus("ComponentStatus") == ComponentStatus::Warning || !filePath.has_value())
+    {
+        writer = std::nullopt;
+        return;
+    }
+    // TODO: Compute CSV header info
+
+    // Replace the csv writer (can it ever survive a reconfigure?)
+    writer.emplace(filePath.value());
+}
+
+void MultiCsvRecorderImpl::reconfigure()
+{
+    auto descriptorList = List<IDataDescriptor>();
+    for (const auto& descriptor : cachedDescriptors)
+        descriptorList.pushBack(descriptor.second);
+    configure(sumDomainDataDescriptor, descriptorList);
+}
+
+void MultiCsvRecorderImpl::onPathChanged()
+{
+    filePath = static_cast<std::string>(objPtr.getPropertyValue(Props::PATH));
+    reconfigure();
+}
+
+void MultiCsvRecorderImpl::onConnected(const InputPortPtr& inputPort)
+{
+    auto lock = this->getAcquisitionLock2();
+
+    LOG_I("Sum Reader FB: Input port {} connected", inputPort.getLocalId())
+
+    if (updateInputPorts())
+    {
+        updateReader();
+    }
+}
+
+void MultiCsvRecorderImpl::onDisconnected(const InputPortPtr& inputPort)
+{
+    auto lock = this->getAcquisitionLock2();
+
+    LOG_I("Sum Reader FB: Input port {} disconnected", inputPort.getLocalId())
+    if (updateInputPorts())
+    {
+        updateReader();
+        reconfigure();
+    }
+}
+
+void MultiCsvRecorderImpl::onDataReceived()
+{
+    auto lock = this->getAcquisitionLock2();
+
+    SizeT cnt = reader.getAvailableCount();
+
+    auto numPorts = connectedPorts.size();
+    std::vector<std::unique_ptr<double[]>> data;
+    data.reserve(numPorts);
+
+    for (size_t i = 0; i < numPorts; ++i)
+        data.push_back(std::make_unique<double[]>(cnt));
+
+    const MultiReaderStatusPtr status = reader.read(data.data(), &cnt);
+
+    if (cnt > 0)
+    {
+        if (recordingActive && writer.has_value())
+        {
+            writer.value().writeSamples(std::move(data), cnt);
+        }
+    }
+
+    if (status.getReadStatus() == ReadStatus::Event)
+    {
+        const auto eventPackets = status.getEventPackets();
+        if (eventPackets.getCount() > 0)
+        {
+            DataDescriptorPtr domainDescriptor;
+            ListPtr<IDataDescriptor> valueDescriptors = List<IDataDescriptor>();
+
+            bool domainChanged = false;
+            bool valueSigChanged = false;
+
+            for (const auto& port : connectedPorts)
+            {
+                auto portGlobalId = port.getGlobalId();
+                DataDescriptorPtr valueDescriptor;
+                if (eventPackets.hasKey(portGlobalId))
+                {
+                    getDataDescriptors(eventPackets.get(portGlobalId), valueDescriptor, domainDescriptor);
+
+                    if (descriptorNotNull(valueDescriptor))
+                    {
+                        valueSigChanged = true;
+                        valueDescriptors.pushBack(valueDescriptor);
+                        cachedDescriptors[portGlobalId] = valueDescriptor;
+                    }
+
+                    domainChanged |= descriptorNotNull(domainDescriptor);
+                }
+
+                if (!descriptorNotNull(valueDescriptor))
+                    valueDescriptors.pushBack(cachedDescriptors[portGlobalId]);
+            }
+
+            getDomainDescriptor(status.getMainDescriptor(), domainDescriptor);
+
+            if (valueSigChanged || domainChanged)
+                configure(domainDescriptor, valueDescriptors);
+
+            if (!status.getValid())
+            {
+                LOG_D("Sum Reader FB: Attempting reader recovery")
+                reader = MultiReaderFromExisting(reader, SampleType::Float64, SampleType::Int64);
+                if (!reader.asPtr<IReaderConfig>().getIsValid())
+                {
+                    setComponentStatusWithMessage(ComponentStatus::Warning, "Reader failed to recover from invalid state!");
+                }
+            }
+        }
+    }
+}
+
+END_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE

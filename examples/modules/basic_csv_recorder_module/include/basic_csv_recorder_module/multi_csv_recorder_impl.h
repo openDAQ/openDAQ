fb58acd2ae9a8a14a8fb7111ef0d2d9ad48c6549
tomaz-cvetko <tomaz.cvetko@opendaq.com>
2025-12-19 07:44:16 +0100

    CSV recorder multi reader mode (#1000)
    * Add a FB with multiple inputs and zero output signals. (#982)
    
    * Add a FB with multiple inputs and zero output signals.
    
    * Write double samples to the same file (no header, no sample type flexibility).
    
    * Subtask: optional domain column, descriptor changes cause opening new files, invalid reader stops the recording (#985)
    
    * Set component state to warning when reader is invalid.
    
    * Write tests for sample rate mismatch detection.
    
    * Add tests for different rate signals.
    
    * Halfrate should be one half of the normal rate.
    
    * Write samples to a single file with header
    
    * Test csv output file contents.
    
    * Add first measurement offset to csv header.
    
    * Column names from signals, not descriptors.
    
    * Provide necessary details for implicit domain rule interpretation
    
    * Write to sequential files
    
    * Code readability
    
    * Review changes: remove redundant code
    
    * Detect descriptor changes that would influence csv header.
    
    * Optionally write timestamp as the first column in the file
    
    * New file on descriptor change and reader recovery with tests.
    
    * Rename property and minor cleanup.
    
    * Changelog update
    
    * Fix failing test - autorecover to recording state after reader was invalidated and recording stopped.

---
+++ examples/modules/basic_csv_recorder_module/include/basic_csv_recorder_module/multi_csv_recorder_impl.h
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2022-2025 openDAQ d.o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <fstream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <thread>
+
+#include <coretypes/filesystem.h>
+#include <opendaq/function_block_impl.h>
+#include <opendaq/opendaq.h>
+
+#include <basic_csv_recorder_module/common.h>
+#include <basic_csv_recorder_module/multi_csv_writer.h>
+
+BEGIN_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE
+
+/*!
+ * @brief A basic recorder function block which records data from its input signals into a CSV file.
+ */
+class MultiCsvRecorderImpl final : public FunctionBlockImpl<IFunctionBlock, IRecorder>
+{
+public:
+    static constexpr const char* TYPE_ID = "MultiCsvRecorder";
+
+    struct Tags
+    {
+        static constexpr const char* RECORDER = "Recorder";
+    };
+
+    struct Props
+    {
+        static constexpr const char* DIR = "Directory";
+        static constexpr const char* BASENAME = "Basename";
+        static constexpr const char* FILE_TIMESTAMP_ENABLED = "FileTimestampEnabled";
+        static constexpr const char* WRITE_DOMAIN = "WriteDomain";
+    };
+
+    /*!
+     * @brief Creates a new function block.
+     * @param context The openDAQ context object.
+     * @param parent The component object which will contain this function block.
+     * @param localId The local identifier of this function block.
+     * @param config A property object containing configuration data for this function block.
+     */
+    MultiCsvRecorderImpl(const ContextPtr& context, const ComponentPtr& parent, const StringPtr& localId, const PropertyObjectPtr& config);
+
+    ~MultiCsvRecorderImpl() = default;
+
+    static FunctionBlockTypePtr createType();
+
+    ErrCode INTERFACE_FUNC startRecording() override;
+    ErrCode INTERFACE_FUNC stopRecording() override;
+    ErrCode INTERFACE_FUNC getIsRecording(Bool* isRecording) override;
+
+protected:
+    virtual void activeChanged() override;
+
+private:
+    void initProperties();
+
+    std::string getNextPortID() const;
+
+    bool updateInputPorts();
+    void updateReader();
+
+    /**
+     * @brief Attempts to open a new CSV writer with provided data.
+     */
+    void configureWriter(const DataDescriptorPtr& domainDescriptor,
+                         const ListPtr<IDataDescriptor>& valueDescriptors,
+                         const ListPtr<IString>& signalNames);
+
+    /**
+     * @brief Collects cached data and calls configure writer.
+     */
+    void reconfigureWriter();
+    void onPropertiesChanged();
+
+    void onConnected(const InputPortPtr& inputPort) override;
+    void onDisconnected(const InputPortPtr& inputPort) override;
+    void onDataReceived();
+    void stopRecordingInternal(bool recover);
+    void startRecordingInternal();
+
+    MultiReaderStatusPtr attemptReadData();
+    bool attemptRecoverReader();
+
+    std::vector<InputPortPtr> connectedPorts;
+    InputPortPtr disconnectedPort;
+
+    std::unordered_map<std::string, DataDescriptorPtr> cachedDescriptors;
+    std::unordered_map<std::string, StringPtr> cachedSignalNames;
+    DataDescriptorPtr recorderDomainDataDescriptor;
+
+    PacketReadyNotification notificationMode;
+    MultiReaderPtr reader;
+
+    bool recordingActive = false;
+    bool recoverToActive = false;
+
+    std::optional<fs::path> filePath = std::nullopt;
+    std::string fileBasename;
+    bool timestampEnabled;
+    bool writeDomain;
+
+    std::optional<MultiCsvWriter> writer = std::nullopt;
+};
+
+END_NAMESPACE_OPENDAQ_BASIC_CSV_RECORDER_MODULE

f570b564e03ffff0c2bb162531578d9ece8f343a
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-01-26 17:08:34 +0100

    Move opcua modules to separate repo

---
+++ modules/opcua_modules/Fetch.cmake
@@ -0,0 +1,7 @@
+opendaq_fetch_module(
+    NAME                OpcUaModules
+#    GIT_REPOSITORY      https://github.com/openDAQ/OpcUaModules.git
+    GIT_REPOSITORY file:///home/nick/dewesoft/blueberry_sdk/OpcUaModules.git
+    GIT_REF             other/extracted-history
+    GIT_SHALLOW         OFF
+)

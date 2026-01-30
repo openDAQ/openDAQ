0420da01e846dc014cdc6cb404a40cc5e749e625
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-01-30 15:10:16 +0100

    Retarget opcua modules to correct repo ref

--- modules/opcua_modules/Fetch.cmake
+++ modules/opcua_modules/Fetch.cmake
@@ -1,7 +1,6 @@
 opendaq_fetch_module(
     NAME                OpcUaModules
-#    GIT_REPOSITORY      https://github.com/openDAQ/OpcUaModules.git
-    GIT_REPOSITORY file:///home/nick/dewesoft/blueberry_sdk/OpcUaModules.git
-    GIT_REF             other/extracted-history
+    GIT_REPOSITORY      https://github.com/openDAQ/OpcUaModules.git
+    GIT_REF             other/complete-missing-files
     GIT_SHALLOW         OFF
 )

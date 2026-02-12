f4af71c3912e554dd8b4f0b96ab731b8159a3921
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-02-12 16:53:52 +0100

    Fix aliasing deprecated cmake funcs and macro
    * Fix vars substitution

--- cmake/DeprecatedCommands.cmake
+++ cmake/DeprecatedCommands.cmake
@@ -1,65 +1,88 @@
 # ----------------------------------------------------------------------
-# Defines a deprecated function alias
-# Parameters:
-#   OLD_NAME - old function name (deprecated)
-#   NEW_NAME - new function name to call
+# Prints a deprecation for aliased function/macro once if called
 # ----------------------------------------------------------------------
-macro(define_deprecated_function_alias OLD_NAME NEW_NAME)
-    if(NOT COMMAND ${NEW_NAME})
-        message(FATAL_ERROR
-            "Deprecated alias ${OLD_NAME}() refers to missing command "
-            "'${NEW_NAME}()'."
-        )
+function(opendaq_show_deprecated_message_once ALIASED_NAME ORIGINAL_NAME)
+    if(NOT DEFINED _${ALIASED_NAME}_DEPRECATED_WARNED)
+        set(_${ALIASED_NAME}_DEPRECATED_WARNED TRUE CACHE INTERNAL "")
+        message(DEPRECATION "${ALIASED_NAME}() is deprecated. Use ${ORIGINAL_NAME}() instead.")
     endif()
-    function(${OLD_NAME})
-        # warn only once
-        if(NOT DEFINED _${OLD_NAME}_DEPRECATED_WARNED)
-            set(_${OLD_NAME}_DEPRECATED_WARNED TRUE CACHE INTERNAL "")
-            message(DEPRECATION "${OLD_NAME}() is deprecated. Use ${NEW_NAME}() instead.")
-        endif()
-        ${NEW_NAME}(${ARGV})
-    endfunction()
+endfunction()
+
+# ----------------------------------------------------------------------
+# Define a deprecated function/macro aliases
+# ----------------------------------------------------------------------
+macro(print_var)
+    opendaq_show_deprecated_message_once(print_var opendaq_print_var)
+    opendaq_print_var(${ARGV})
 endmacro()
 
-# Functions
-define_deprecated_function_alias(print_var 				opendaq_print_var)
-define_deprecated_function_alias(get_current_folder 			opendaq_get_current_folder)
-define_deprecated_function_alias(set_cmake_folder_context 		opendaq_set_cmake_folder_context)
-define_deprecated_function_alias(set_cmake_context 			opendaq_set_cmake_context)
-define_deprecated_function_alias(set_cmake_folder 			opendaq_set_cmake_folder)
-define_deprecated_function_alias(prepend_include 			opendaq_prepend_include)
-define_deprecated_function_alias(create_version_header 		opendaq_generate_version_header)
-define_deprecated_function_alias(check_if_files_exist 		opendaq_check_if_files_exist)
-define_deprecated_function_alias(dump_cmake_variables 		opendaq_dump_cmake_variables)
-define_deprecated_function_alias(use_compiler_cache 			opendaq_use_compiler_cache)
-define_deprecated_function_alias(get_custom_fetch_content_params 	opendaq_get_custom_fetch_content_params)
-define_deprecated_function_alias(add_cmake_targets 			opendaq_add_cmake_targets)
-
-
-# ----------------------------------------------------------------------
-# Defines a deprecated macro alias
-# Parameters:
-#   OLD_NAME - old macro name (deprecated)
-#   NEW_NAME - new macro name to call
-# ----------------------------------------------------------------------
-macro(define_deprecated_macro_alias OLD_NAME NEW_NAME)
-    if(NOT COMMAND ${NEW_NAME})
-        message(FATAL_ERROR
-            "Deprecated alias ${OLD_NAME}() refers to missing command "
-            "'${NEW_NAME}()'."
-        )
-    endif()
-    macro(${OLD_NAME})
-        # warn only once
-        if(NOT DEFINED _${OLD_NAME}_DEPRECATED_WARNED)
-            set(_${OLD_NAME}_DEPRECATED_WARNED TRUE CACHE INTERNAL "")
-            message(DEPRECATION "${OLD_NAME}() is deprecated. Use ${NEW_NAME}() instead.")
-        endif()
-        ${NEW_NAME}(${ARGV})
-    endmacro()
+macro(get_current_folder)
+    opendaq_show_deprecated_message_once(get_current_folder opendaq_get_current_folder)
+    opendaq_get_current_folder(${ARGV})
 endmacro()
 
-# Macro
-define_deprecated_macro_alias	 (is_git_repository_root 		opendaq_is_git_repository_root)
-define_deprecated_macro_alias	 (add_application_option 		opendaq_add_application_option)
+macro(get_current_folder_name)
+    opendaq_show_deprecated_message_once(get_current_folder_name opendaq_get_current_folder_name)
+    opendaq_get_current_folder_name(${ARGV})
+endmacro()
+
+macro(set_cmake_folder_context)
+    opendaq_show_deprecated_message_once(set_cmake_folder_context opendaq_set_cmake_folder_context)
+    opendaq_set_cmake_folder_context(${ARGV})
+endmacro()
+
+macro(set_cmake_context)
+    opendaq_show_deprecated_message_once(set_cmake_context opendaq_set_cmake_context)
+    opendaq_set_cmake_context(${ARGV})
+endmacro()
+
+macro(set_cmake_folder)
+    opendaq_show_deprecated_message_once(set_cmake_folder opendaq_set_cmake_folder)
+    opendaq_set_cmake_folder(${ARGV})
+endmacro()
+
+macro(prepend_include)
+    opendaq_show_deprecated_message_once(prepend_include opendaq_prepend_include)
+    opendaq_prepend_include(${ARGV})
+endmacro()
+
+macro(create_version_header)
+    opendaq_show_deprecated_message_once(create_version_header opendaq_generate_version_header)
+    opendaq_generate_version_header(${ARGV})
+endmacro()
+
+macro(check_if_files_exist)
+    opendaq_show_deprecated_message_once(check_if_files_exist opendaq_check_if_files_exist)
+    opendaq_check_if_files_exist(${ARGV})
+endmacro()
+
+macro(dump_cmake_variables)
+    opendaq_show_deprecated_message_once(dump_cmake_variables opendaq_dump_cmake_variables)
+    opendaq_dump_cmake_variables(${ARGV})
+endmacro()
+
+macro(use_compiler_cache)
+    opendaq_show_deprecated_message_once(use_compiler_cache opendaq_use_compiler_cache)
+    opendaq_use_compiler_cache(${ARGV})
+endmacro()
+
+macro(get_custom_fetch_content_params)
+    opendaq_show_deprecated_message_once(get_custom_fetch_content_params opendaq_get_custom_fetch_content_params)
+    opendaq_get_custom_fetch_content_params(${ARGV})
+endmacro()
+
+macro(add_cmake_targets)
+    opendaq_show_deprecated_message_once(add_cmake_targets opendaq_add_cmake_targets)
+    opendaq_add_cmake_targets(${ARGV})
+endmacro()
+
+macro(is_git_repository_root)
+    opendaq_show_deprecated_message_once(is_git_repository_root opendaq_is_git_repository_root)
+    opendaq_is_git_repository_root(${ARGV})
+endmacro()
+
+macro(add_application_option)
+    opendaq_show_deprecated_message_once(add_application_option opendaq_add_application_option)
+    opendaq_add_application_option(${ARGV})
+endmacro()
 

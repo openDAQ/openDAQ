2897796800416d498ea21b153b89c7ce6d245c43
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-02-18 08:13:59 +0100

    Clean-up example modules

--- examples/modules/empty_module/modules/empty_module/tests/test_app.cpp
+++ examples/modules/empty_module/modules/empty_module/tests/test_app.cpp
@@ -1,15 +1,12 @@
+#include <opendaq/module_manager_factory.h>
 #include <testutils/testutils.h>
 #include <testutils/daq_memcheck_listener.h>
 
-#include <coreobjects/util.h>
-#include <opendaq/module_manager_init.h>
-#include <coretypes/stringobject_factory.h>
-
 int main(int argc, char** args)
 {
-    daq::daqInitializeCoreObjectsTesting();
-    daqInitModuleManagerLibrary();
-
+    {
+        daq::ModuleManager(".");
+    }
     testing::InitGoogleTest(&argc, args);
 
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();

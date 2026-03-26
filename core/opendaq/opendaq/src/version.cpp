9a582ee3613e52e9daee7918a03b03716032a51a
Nikolai Shipilov <nikolai.shipilov@opendaq.com>
2026-03-26 08:06:59 +0100

    Revert providing core version numbers

--- core/opendaq/opendaq/src/version.cpp
+++ core/opendaq/opendaq/src/version.cpp
@@ -4,7 +4,7 @@
 extern "C"
 void PUBLIC_EXPORT daqOpenDaqGetVersion(unsigned int* major, unsigned int* minor, unsigned int* revision)
 {
-    *major = 0u;
-    *minor = 0u;
-    *revision = 0u;
+    *major = OPENDAQ_OPENDAQ_MAJOR_VERSION;
+    *minor = OPENDAQ_OPENDAQ_MINOR_VERSION;
+    *revision = OPENDAQ_OPENDAQ_PATCH_VERSION;
 }

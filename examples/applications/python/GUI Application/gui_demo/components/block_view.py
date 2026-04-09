7fc69939422b7176e7d73962df5d9126cb567af5
Maksim Loknar <loknarmaksim@gmail.com>
2026-04-09 13:50:41 +0200

    Fixed right_side_view properties sizing

--- examples/applications/python/GUI Application/gui_demo/components/block_view.py
+++ examples/applications/python/GUI Application/gui_demo/components/block_view.py
@@ -356,7 +356,7 @@ class BlockView(ttk.Frame):
             self.expanded_frame.grid_columnconfigure(
                 self.cols, weight=1, minsize=int(200 * self.context.dpi_factor), uniform='column')
             self.expanded_frame.grid_rowconfigure(self.rows, weight=1,
-                                                  minsize=int(600 * self.context.dpi_factor) if self.input_ports and self.output_signals
+                                                  minsize=int(350 * self.context.dpi_factor) if self.input_ports and self.output_signals
                                                                  or daq.IFolder.can_cast_from(self.node) and
                                                                  not daq.IDevice.can_cast_from(self.node) else int(600 * self.context.dpi_factor))
             if self.properties:

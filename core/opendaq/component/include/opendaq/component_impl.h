7339ff7a2377f5e380af3665ee8a12333b213c32
Denis Erokhin <149682271+denise-opendaq@users.noreply.github.com>
2026-04-29 15:17:22 +0200

    Reduce component config size (#1179)

--- core/opendaq/component/include/opendaq/component_impl.h
+++ core/opendaq/component/include/opendaq/component_impl.h
@@ -1280,7 +1280,8 @@ void ComponentImpl<Intf, Intfs...>::serializeCustomObjectValues(const Serializer
         tags.serialize(serializer);
     }
 
-    if(!forUpdate) {
+    if (!forUpdate)
+    {
         if (statusContainer.getStatuses().getCount() > 0)
         {
             serializer.key("statuses");
@@ -1291,7 +1292,10 @@ void ComponentImpl<Intf, Intfs...>::serializeCustomObjectValues(const Serializer
     if (componentConfig.assigned())
     {
         serializer.key("ComponentConfig");
-        componentConfig.serialize(serializer);
+        if (forUpdate)
+            componentConfig.template asPtr<IUpdatable>(true).serializeForUpdate(serializer);
+        else
+            componentConfig.serialize(serializer);
     }
 }
 

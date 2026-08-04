b1ae64e08c07d8d6d9c42b3fbbb89764962b7a3a
Denis Erokhin <denis.erokhin@opendaq.com>
2026-08-04 10:06:08 +0200

    last polish

--- shared/libraries/config_protocol/include/config_protocol/config_client_device_impl.h
+++ shared/libraries/config_protocol/include/config_protocol/config_client_device_impl.h
@@ -74,7 +74,7 @@ public:
     ErrCode INTERFACE_FUNC setParentActive(Bool parentActive, Bool onUpdate) override;
     ErrCode INTERFACE_FUNC setAsRoot() override;
 
-    ErrCode INTERFACE_FUNC getInfo(IDeviceInfo** info) override;
+    DeviceInfoPtr onGetInfo() override;
 
     template <class Implementation>
     static ErrCode Deserialize(ISerializedObject* serialized, IBaseObject* context, IFunction* factoryCallback, IBaseObject** obj);
@@ -408,29 +408,9 @@ ErrCode GenericConfigClientDeviceImpl<TDeviceBase>::setAsRoot()
 }
 
 template <class TDeviceBase>
-ErrCode GenericConfigClientDeviceImpl<TDeviceBase>::getInfo(IDeviceInfo** info)
+DeviceInfoPtr GenericConfigClientDeviceImpl<TDeviceBase>::onGetInfo()
 {
-    OPENDAQ_PARAM_NOT_NULL(info);
-
-    if (this->isComponentRemoved)
-        return DAQ_MAKE_ERROR_INFO(OPENDAQ_ERR_COMPONENT_REMOVED);
-
-    // Remote config client: DeviceInfo lives in the DaqDeviceInfo property (deserialized / updated from server).
-    this->deviceInfo = this->objPtr.getPropertyValue("DaqDeviceInfo").template asPtrOrNull<IDeviceInfo>(true);
-
-    DeviceTypePtr mirroredDeviceType;
-    OPENDAQ_RETURN_IF_FAILED(this->getMirroredDeviceType(&mirroredDeviceType));
-
-    if (this->deviceInfo.assigned() && mirroredDeviceType.assigned() && !this->deviceInfo.getDeviceType().assigned())
-    {
-        const ErrCode setErr =
-            this->deviceInfo.template asPtr<IDeviceInfoConfig>(true)->setDeviceType(mirroredDeviceType);
-        if (OPENDAQ_FAILED(setErr))
-            daqClearErrorInfo();
-    }
-
-    *info = this->deviceInfo.addRefAndReturn();
-    return OPENDAQ_SUCCESS;
+    return this->objPtr.getPropertyValue("DaqDeviceInfo").template asPtrOrNull<IDeviceInfo>(true);
 }
 
 template <class TDeviceBase>

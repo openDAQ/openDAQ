d05c2dfbddae76509f997f840cd30ac1e8a376a0
Maksim Loknar <loknarmaksim@gmail.com>
2026-08-26 22:08:20 +0200

    Adjusted the dialogs to make more sence in the new usecase

--- examples/applications/python/GUI Application/gui_demo/components/add_function_block_dialog.py
+++ examples/applications/python/GUI Application/gui_demo/components/add_function_block_dialog.py
@@ -18,30 +18,8 @@ class AddFunctionBlockDialog(Dialog):
         self.parent_component = selected_component
 
         self.geometry('{}x{}'.format(
-            int(1400 * self.context.ui_scaling_factor * self.context.dpi_factor),
+            int(1000 * self.context.ui_scaling_factor * self.context.dpi_factor),
             int(400 * self.context.ui_scaling_factor * self.context.dpi_factor)))
-
-        # parent
-
-        parent_device_tree_frame = ttk.Frame(self)
-        parent_device_tree = ttk.Treeview(parent_device_tree_frame)
-
-        parent_device_scroll_bar = ttk.Scrollbar(
-            parent_device_tree_frame, orient=tk.VERTICAL, command=parent_device_tree.yview)
-        parent_device_tree.configure(
-            yscrollcommand=parent_device_scroll_bar.set)
-        parent_device_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        parent_device_tree.heading('#0', text='Parent', anchor=tk.W)
-
-        parent_device_tree.column(
-            '#0', anchor=tk.W, minwidth=int(200 * self.context.dpi_factor), stretch=True)
-
-        parent_device_tree.bind('<<TreeviewSelect>>',
-                                self.handle_parent_device_selected)
-        parent_device_tree.pack(fill=tk.BOTH, expand=True)
-
-        parent_device_tree_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
         # child
 
@@ -75,15 +53,15 @@ class AddFunctionBlockDialog(Dialog):
 
         tree.pack(fill=tk.BOTH, expand=True)
 
-        tree_frame.grid(row=0, column=1, sticky=tk.NSEW)
+        tree_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.device_tree = parent_device_tree
         self.fb_tree = tree
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=2)
-        self.grid_columnconfigure((0, 1), uniform='uniform')
+
+        if self.parent_component is not None:
+            self.title('Add function block to {}'.format(self.parent_component.name))
 
         actions_row = ttk.Frame(tree_frame)
         self._keep_open_var = tk.BooleanVar(value=False)
@@ -100,41 +78,9 @@ class AddFunctionBlockDialog(Dialog):
         self.update_dialog()
 
     def update_dialog(self):
-        self.update_parent_devices(
-            self.device_tree, '', self.context.instance)
-        parent_to_select = self.parent_component.global_id if self.parent_component is not None else self.context.instance.global_id
-        self.select_parent_device(parent_to_select)
-
-    def select_parent_device(self, device_id: str):
-        if self.device_tree.exists(device_id):
-            self.device_tree.selection_set(device_id)
-
-    def update_parent_devices(self, tree, parent_id, component):
-        tree.delete(*tree.get_children())
-
-        def traverse_devices_recursive(tree, parent_id, component):
-            if component is None:
-                return
-
-            if daq.IDevice.can_cast_from(component):
-                device = daq.IDevice.cast_from(component)
-                tree.insert(parent_id, tk.END, text=device.name,
-                            iid=device.global_id, open=tk.TRUE)
-                parent_id = device.global_id
-
-            if daq.IFunctionBlock.can_cast_from(component):
-                function_block = daq.IFunctionBlock.cast_from(component)
-                if function_block.available_function_block_types:
-                    tree.insert(parent_id, tk.END, text=function_block.name,
-                                iid=function_block.global_id, open=tk.TRUE)
-                    parent_id = function_block.global_id
-
-            if daq.IFolder.can_cast_from(component):
-                folder = daq.IFolder.cast_from(component)
-                for item in folder.items:
-                    traverse_devices_recursive(tree, parent_id, item)
-
-        traverse_devices_recursive(tree, parent_id, component)
+        if self.parent_component is None:
+            self.parent_component = self.context.instance
+        self.update_function_blocks()
 
     def update_function_blocks(self):
         self.fb_tree.delete(*self.fb_tree.get_children())
@@ -144,20 +90,6 @@ class AddFunctionBlockDialog(Dialog):
             fb_type = daq.IFunctionBlockType.cast_from(available_function_block_types[function_block_id])
             self.fb_tree.insert('', tk.END, iid=function_block_id, values=(
                 fb_type.name, fb_type.description, function_block_id))
-
-    def handle_parent_device_selected(self, event):
-        selected_item = utils.treeview_get_first_selection(
-            self.device_tree)
-        if selected_item is None:
-            return
-
-        parent_component = utils.find_component(
-            selected_item, self.context.instance)
-        if parent_component is not None:
-            if daq.IDevice.can_cast_from(parent_component) or daq.IFunctionBlock.can_cast_from(parent_component):
-                self.parent_component = daq.IDevice.cast_from(parent_component) if daq.IDevice.can_cast_from(
-                    parent_component) else daq.IFunctionBlock.cast_from(parent_component)
-                self.update_function_blocks()
 
     def handle_fb_type_selected(self, e=None):
         can_config = False
